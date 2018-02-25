a6fee29782ddfeb197f31a90ff761a6667f7311c
Pascal Honoré <pascal@alseyn.net>
2018-02-25 14:38:58 +0000

    Update in Ninja's metric computation

--- Ninja.rb
+++ Ninja.rb
@@ -12,7 +12,7 @@ class Ninja
         objects = []
         pendingcount = `/Galaxy/LucilleOS/Binaries/ninja api:catalyst:pendingcount`.to_i
         dayactivitycount = `/Galaxy/LucilleOS/Binaries/ninja api:catalyst:last-24-hours-activity-count`.to_i
-        metric = pendingcount==0 ? 0 : 0.3*(1-Math.exp(-pendingcount.to_f/50))*Math.exp(-dayactivitycount.to_f/50) 
+        metric = pendingcount==0 ? 0 : 0.2 + 0.1*Math.exp(-dayactivitycount.to_f/20)
             # The second term raise the metric from 0 to 1 as the pending count increases
             # The third term collapse from 1 to 0 as the last-24-hours-activity-count raises
         objects << {
