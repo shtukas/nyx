58a1c9af34ae36cd6a0bb4840622aa97565a9923
Pascal Honoré <pascal@alseyn.net>
2018-03-27 13:02:15 +0100

    Updates on Torr functionalities

--- Torr.rb
+++ Torr.rb
@@ -29,6 +29,7 @@ require 'securerandom'
 # -------------------------------------------------------------------------------------
 
 TORR_PATH_TO_STREAM_ITEM_FOLDER = "/Galaxy/DataBank/Catalyst/Stream/items"
+TORR_PATH_TO_TORR_ITEM_FOLDER = "/Galaxy/DataBank/Catalyst/Torr/items"
 
 # Torr::itemsFolderpath()
 # Torr::getItemDescription(folderpath)
@@ -39,31 +40,38 @@ TORR_PATH_TO_STREAM_ITEM_FOLDER = "/Galaxy/DataBank/Catalyst/Stream/items"
 class Torr
 
     def self.itemsFolderpath()
-        Dir.entries(TORR_PATH_TO_STREAM_ITEM_FOLDER)
+        f1 = Dir.entries(TORR_PATH_TO_STREAM_ITEM_FOLDER)
             .select{|filename| filename[0,1]!='.' }
             .sort
             .map{|filename| "#{TORR_PATH_TO_STREAM_ITEM_FOLDER}/#{filename}" }
             .select{|folderpath| File.exist?("#{folderpath}/.torr") }
+        f2 = Dir.entries(TORR_PATH_TO_TORR_ITEM_FOLDER)
+            .select{|filename| filename[0,1]!='.' }
+            .sort
+            .map{|filename| "#{TORR_PATH_TO_TORR_ITEM_FOLDER}/#{filename}" }
+        f1+f2
     end
 
     def self.getItemDescription(folderpath)
         uuid = IO.read("#{folderpath}/.uuid").strip
         description = KeyValueStore::getOrDefaultValue(nil, "c441a43a-bb70-4850-b23c-1db5f5665c9a:#{uuid}", "#{folderpath}")
     end
-
+    def self.uuid2metricuuid(uuid)
+        "#{uuid}:#{Today.new.to_s[0,10]}"
+    end
     def self.pathToItemToCatalystObject(folderpath, individualItemDailyCommitmentInHours)
         if !File.exist?("#{folderpath}/.uuid") then
             File.open("#{folderpath}/.uuid", 'w'){|f| f.puts(SecureRandom.hex(4)) }
         end
         uuid = IO.read("#{folderpath}/.uuid").strip
         description = Torr::getItemDescription(folderpath)
-        metric = DRbObject.new(nil, "druby://:10423").metric2("#{uuid}:#{Today.new.to_s[0,10]}", 1, individualItemDailyCommitmentInHours, 0.6, 0.8, 2)
+        metric = DRbObject.new(nil, "druby://:10423").metric2(Torr::uuid2metricuuid(uuid), 1, individualItemDailyCommitmentInHours, 0.6, 0.8, 2)
         {
             "uuid" => uuid,
             "metric" => metric,
-            "announce" => "(#{"%.3f" % metric}) torr: #{description} (#{"%.2f" % ( DRbObject.new(nil, "druby://:10423").getEntityTotalTimespanForPeriod("#{uuid}:#{Today.new.to_s[0,10]}", 7).to_f/3600 )} hours)",
-            "commands" => ["start", "stop", "folder", "set-description"],
-            "default-commands" => DRbObject.new(nil, "druby://:10423").isRunning("#{uuid}:#{Today.new.to_s[0,10]}") ? ['stop'] : ['start'],
+            "announce" => "(#{"%.3f" % metric}) torr: #{description} (#{"%.2f" % ( DRbObject.new(nil, "druby://:10423").getEntityTotalTimespanForPeriod(Torr::uuid2metricuuid(uuid), 7).to_f/3600 )} hours)",
+            "commands" => ["start", "stop", "folder", "set-description", "completed"],
+            "default-commands" => DRbObject.new(nil, "druby://:10423").isRunning(Torr::uuid2metricuuid(uuid)) ? ['stop'] : ['start'],
             "command-interpreter" => lambda{|object, command| Torr::objectCommandHandler(object, command) },
             "item-folderpath" => folderpath
         }
@@ -75,18 +83,34 @@ class Torr
             return
         end
         if command=='start' then
-            DRbObject.new(nil, "druby://:10423").start("#{object['uuid']}:#{Today.new.to_s[0,10]}")
+            uuid = object['uuid']
+            DRbObject.new(nil, "druby://:10423").start(Torr::uuid2metricuuid(uuid))
             system("open '#{object['item-folderpath']}'")
             return
         end
         if command=='stop' then
-            DRbObject.new(nil, "druby://:10423").stopAndAddTimeSpan("#{object['uuid']}:#{Today.new.to_s[0,10]}")
+            uuid = object['uuid']
+            DRbObject.new(nil, "druby://:10423").stopAndAddTimeSpan(Torr::uuid2metricuuid(uuid))
             return
         end
         if command=='set-description' then
-            description = LucilleCore::askQuestionAnswerAsString("description: ")
             uuid = object['uuid']
+            description = LucilleCore::askQuestionAnswerAsString("description: ")
             KeyValueStore::set(nil, "c441a43a-bb70-4850-b23c-1db5f5665c9a:#{uuid}", "#{description}")
+            return
+        end
+        if command=="completed" then
+            uuid = object['uuid']
+            if DRbObject.new(nil, "druby://:10423").isRunning(Torr::uuid2metricuuid(uuid)) then
+                DRbObject.new(nil, "druby://:10423").stopAndAddTimeSpan(Torr::uuid2metricuuid(uuid))
+            end
+            time = Time.new
+            targetFolder = "/Galaxy/DataBank/Catalyst/ArchivesTimeline/#{time.strftime("%Y")}/#{time.strftime("%Y%m")}/#{time.strftime("%Y%m%d")}/#{time.strftime("%Y%m%d-%H%M%S-%6N")}/"
+            puts "Source: #{object['item-folderpath']}"
+            puts "Target: #{targetFolder}"
+            FileUtils.mkpath(targetFolder)
+            FileUtils.mv("#{object['item-folderpath']}",targetFolder)
+            LucilleCore::removeFileSystemLocation(object['item-folderpath'])
             return
         end
     end
