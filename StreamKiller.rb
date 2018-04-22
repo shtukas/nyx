f6b916893f52eba5b6c8c545441dbb77ecf39752
Pascal Honoré <pascal@alseyn.net>
2018-04-22 07:23:06 +0100

    Better stream killer metric

--- StreamKiller.rb
+++ StreamKiller.rb
@@ -51,35 +51,52 @@ require "/Galaxy/local-resources/Ruby-Libraries/KeyValueStore.rb"
 
 class StreamKiller
     def self.getCatalystObjects()
+
         startingUnixtime = 1524341098
         endingUnixtime   = 1524341098 + 86400*100 # 100 days
         startingCount    = 995
         endingCount      = 0
         idealCount       = 995 - 995*(Time.new.to_i - startingUnixtime).to_f/(endingUnixtime - startingUnixtime)
+        
+                # metric = f(idealCount*0.9) = 0
+                #          f(idealCount)     = 1
+                #          slope = 1.to_f/(0.1*idealCount)
+                #          f(x) = x.to_f/(0.1*idealCount) + something
+                #          something = f(x) - x.to_f/(0.1*idealCount)
+                #          something = - (idealCount*0.9).to_f/(0.1*idealCount)
+                #          f(x) = x.to_f/(0.1*idealCount) - (idealCount*0.9).to_f/(0.1*idealCount)
+                #          check: f(idealCount*0.9) = (idealCount*0.9).to_f/(0.1*idealCount) - (idealCount*0.9).to_f/(0.1*idealCount) = 0
+                #          check: f(idealCount)     = idealCount.to_f/(0.1*idealCount) - (idealCount*0.9).to_f/(0.1*idealCount)
+                #                                   = (idealCount*0.9 + idealCount*0.1).to_f/(0.1*idealCount) - (idealCount*0.9).to_f/(0.1*idealCount)
+                #                                   = 1
+
         currentCount     = Dir.entries("/Galaxy/DataBank/Catalyst/Stream/strm1").size
+
+        metric           = currentCount.to_f/(0.1*idealCount) - (idealCount*0.9).to_f/(0.1*idealCount)
+        metric           = [metric, 1].min
+        metric           = [metric, 0].max
+
         targetFoldername = Dir.entries("/Galaxy/DataBank/Catalyst/Stream/strm1").select{|filename| filename[0,1]!="." }.sample
         targetFolderUUID = IO.read("/Galaxy/DataBank/Catalyst/Stream/strm1/#{targetFoldername}/.uuid").strip
         objects = []
-        if currentCount > idealCount then
-            objects << {
-                "uuid" => "2662371C-44C0-422B-83FF-FAB12B76FDED",
-                "metric" => 1,
-                "announce" => "-> stream killer (ideal: #{idealCount}, current: #{currentCount}): /Galaxy/DataBank/Catalyst/Stream/strm1/#{targetFoldername}",
-                "commands" => [],
-                "command-interpreter" => lambda{|object, command| 
-                    targetuuid = object["target-uuid"]
-                    searchobjects = CatalystCore::objects()
-                        .select{|object| object['announce'].downcase.include?(targetuuid) }
-                    if searchobjects.size>0 then
-                        searchobject = searchobjects.first
-                        Jupiter::putsObjectWithShellDisplay(searchobject, [])
-                    end
-                    return [nil, false]
+        objects << {
+            "uuid" => "2662371C-44C0-422B-83FF-FAB12B76FDED",
+            "metric" => metric,
+            "announce" => "(#{"%.3f" % metric}) -> stream killer (ideal: #{idealCount}, current: #{currentCount}): /Galaxy/DataBank/Catalyst/Stream/strm1/#{targetFoldername}",
+            "commands" => [],
+            "command-interpreter" => lambda{|object, command| 
+                targetuuid = object["target-uuid"]
+                searchobjects = CatalystCore::objects()
+                    .select{|object| object['announce'].downcase.include?(targetuuid) }
+                if searchobjects.size>0 then
+                    searchobject = searchobjects.first
+                    Jupiter::putsObjectWithShellDisplay(searchobject, [])
+                end
+                return [nil, false]
 
-                },
-                "target-uuid" => targetFolderUUID
-            }
-        end
+            },
+            "target-uuid" => targetFolderUUID
+        }
         objects
     end
 end
