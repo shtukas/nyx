f71592705a8b6c33644dde3491da44d90b58dd9b
Pascal Honoré <pascal@alseyn.net>
2018-04-23 12:46:05 +0100

    TimeCommitments metric update

--- TimeCommitments.rb
+++ TimeCommitments.rb
@@ -153,7 +153,7 @@ class TimeCommitments
         .map{|item|
             uuid = item['uuid']
             ratioDone = (TimeCommitments::itemToLiveTimespan(item).to_f/3600)/item["commitment-in-hours"]
-            metric = item['metric'] ? item['metric'] : ( 0.810 + Math.exp(ratioDone).to_f/1000 )
+            metric = item["is-running"] ? 2 : ( item['metric'] ? item['metric'] : ( 0.810 + Math.exp(ratioDone).to_f/1000 ) )
             announce = "[#{uuid}] time commitment: #{item['description']} (#{ "%.2f" % (100*ratioDone) } % of #{item["commitment-in-hours"]} hours done)"
             announce = item["is-running"] ? announce.green : announce
             commands = item["is-running"] ? ["stop"] : ["start"]
