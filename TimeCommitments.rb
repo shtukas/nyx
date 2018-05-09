cd793998914702b5fdbcb35d763f5fa761c086a8
Pascal Honoré <pascal@alseyn.net>
2018-05-09 12:39:23 +0100

    TimeCommitments: announce string update

--- TimeCommitments.rb
+++ TimeCommitments.rb
@@ -189,7 +189,7 @@ class TimeCommitments
             uuid = item['uuid']
             ratioDone = (TimeCommitments::itemToLiveTimespan(item).to_f/3600)/item["commitment-in-hours"]
             metric = item['metric'] ? item['metric'] : ( 0.810 + Math.exp(ratioDone).to_f/1000 )
-            announce = "[#{uuid}] time commitment: #{item['description']} (#{ "%.2f" % (100*ratioDone) } % of #{item["commitment-in-hours"]} hours done)"
+            announce = "time commitment: #{item['description']} (#{ "%.2f" % (100*ratioDone) } % of #{item["commitment-in-hours"]} hours done)"
             announce = item["is-running"] ? announce.green : announce
             commands = item["is-running"] ? ["stop", "stop+"] : ["start"]
             defaultExpression = item["is-running"] ? "stop+" : "start"
