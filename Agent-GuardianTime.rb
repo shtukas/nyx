2e215c9ad19943b99f7d117c87e16794c871eabe
Pascal Honoré <pascal@alseyn.net>
2018-05-17 17:40:33 +0100

    GuardianTime is flock ready

--- Agent-GuardianTime.rb
+++ Agent-GuardianTime.rb
@@ -23,7 +23,6 @@ class GuardianTime
     end
 
     def self.flockGeneralUpgrade(flock)
-        return [flock, []]
         if KeyValueStore::getOrNull(CATALYST_COMMON_XCACHE_REPOSITORY, "23ed1630-7c94-47b4-b50e-905a3e5f862a:#{Time.new.to_s[0,10]}").nil? and ![6,0].include?(Time.new.wday) and Time.new.hour>=8 then
             numberOfHours = LucilleCore::askQuestionAnswerAsString("Number of Guardian hours for today (empty default to 5): ")
             if numberOfHours.strip.size==0 then
@@ -43,9 +42,10 @@ class GuardianTime
             end
             KeyValueStore::set(CATALYST_COMMON_XCACHE_REPOSITORY, "23ed1630-7c94-47b4-b50e-905a3e5f862a:#{Time.new.to_s[0,10]}","done")
         end
+        return [flock, []] # We do not return anything else. The side effect is the creation of the TimeCommitment Items
     end
 
     def self.upgradeFlockUsingObjectAndCommand(flock, object, command)
-        return [flock, []]
+        return [flock, []] # Nothing to do, this agent doesn't create objects that it handles itself.
     end
 end
