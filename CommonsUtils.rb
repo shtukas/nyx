3a427cd1b84c5954a4c582ec889b5d7cc96d4826
Pascal Honoré <pascal@alseyn.net>
2018-06-06 03:50:32 +0100

    Flock Service (the way it should have been first implemented) 3/.

--- CommonsUtils.rb
+++ CommonsUtils.rb
@@ -623,4 +623,20 @@ class CommonsUtils
         targetFolder       
     end
 
+    def self.fDoNotShowUntilDateTimeTransform()
+        FlockOperator::flockObjects().map{|object|
+            if !FlockOperator::getDoNotShowUntilDateTimeDistribution()[object["uuid"]].nil? and (Time.new.to_s < FlockOperator::getDoNotShowUntilDateTimeDistribution()[object["uuid"]]) and object["metric"]<=1 then
+                # The second condition in case we start running an object that wasn't scheduled to be shown today (they can be found through search)
+                object["do-not-show-until-datetime"] = FlockOperator::getDoNotShowUntilDateTimeDistribution()[object["uuid"]]
+                object["metric"] = 0
+            end
+            if object["agent-uid"]=="283d34dd-c871-4a55-8610-31e7c762fb0d" and object["schedule"]["do-not-show-until-datetime"] and (Time.new.to_s < object["schedule"]["do-not-show-until-datetime"]) and object["metric"]<=1 then
+                # The second condition in case we start running an object that wasn't scheduled to be shown today (they can be found through search)
+                object["do-not-show-until-datetime"] = object["schedule"]["do-not-show-until-datetime"]
+                object["metric"] = 0
+            end
+            FlockOperator::addOrUpdateObject(object)
+        }
+    end
+
 end
