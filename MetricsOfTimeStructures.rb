703f9c3c288e54be16691f75b15024be0aa6864d
Pascal Honoré <pascal@alseyn.net>
2018-07-10 21:04:31 +0100

    corrections

--- MetricsOfTimeStructures.rb
+++ MetricsOfTimeStructures.rb
@@ -5,6 +5,7 @@
 
 class MetricsOfTimeStructures
     def self.metric2(uuid, donemetric, lowmetric, highmetric, timestructure)
+        return highmetric if Chronos::isRunning(uuid)
         return donemetric if Chronos::ratioDone(uuid, timestructure["time-unit-in-days"], timestructure["time-commitment-in-hours"]) >= 1
         metric1 = MetricsOfChronos::metric3(uuid, lowmetric, highmetric, timestructure["time-unit-in-days"], timestructure["time-commitment-in-hours"])
         metric2 = MetricsOfChronos::metric3(uuid, lowmetric, highmetric, 1, timestructure["time-commitment-in-hours"].to_f/timestructure["time-unit-in-days"]) 
