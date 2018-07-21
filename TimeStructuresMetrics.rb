e5904a9596790ef0be78aaa681ec079f95b764cf
Pascal Honoré <pascal@alseyn.net>
2018-07-21 23:46:09 +0100

    Refactoring

--- TimeStructuresMetrics.rb
+++ TimeStructuresMetrics.rb
@@ -1,11 +1,11 @@
 
 # encoding: UTF-8
 
-# MetricsOfTimeStructures::metric1(low, high, ratiodone)
-# MetricsOfTimeStructures::metric2(uuid, low, high, timeUnitInDays, timeCommitmentInHours)
-# MetricsOfTimeStructures::metric4(uuid, donemetric, lowmetric, highmetric, timestructure)
+# TimeStructuresMetrics::metric1(low, high, ratiodone)
+# TimeStructuresMetrics::metric2(uuid, low, high, timeUnitInDays, timeCommitmentInHours)
+# TimeStructuresMetrics::metric4(uuid, donemetric, lowmetric, highmetric, timestructure)
 
-class MetricsOfTimeStructures
+class TimeStructuresMetrics
     def self.metric1(low, high, ratiodone)
         return low if ratiodone >= 1 
         low + (high-low)*(1-ratiodone)
@@ -14,12 +14,12 @@ class MetricsOfTimeStructures
     def self.metric2(uuid, low, high, timestructure)
         ratiodone1 = Chronos::ratioDone(uuid, timestructure["time-unit-in-days"], timestructure["time-commitment-in-hours"])
         ratiodone2 = Chronos::ratioDone(uuid, 1, timestructure["time-commitment-in-hours"].to_f/timestructure["time-unit-in-days"])
-        MetricsOfTimeStructures::metric1(low, high, [ratiodone1, ratiodone2].max)
+        TimeStructuresMetrics::metric1(low, high, [ratiodone1, ratiodone2].max)
     end
 
     def self.metric4(uuid, donemetric, lowmetric, highmetric, timestructure)
         return highmetric if Chronos::isRunning(uuid)
         return donemetric if Chronos::ratioDone(uuid, timestructure["time-unit-in-days"], timestructure["time-commitment-in-hours"]) >= 1
-        MetricsOfTimeStructures::metric2(uuid, lowmetric, highmetric, timestructure)
+        TimeStructuresMetrics::metric2(uuid, lowmetric, highmetric, timestructure)
     end
 end
