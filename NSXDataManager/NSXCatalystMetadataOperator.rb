1e1155d78f198a719772291a6e09b599d19756c0
Pascal Honoré <pascal@alseyn.net>
2018-10-19 22:05:40 +0100

    Migrating NSXCatalystMetadataOperator to Iphetra

--- NSXDataManager/NSXCatalystMetadataOperator.rb
+++ NSXDataManager/NSXCatalystMetadataOperator.rb
@@ -16,52 +16,22 @@ require 'fileutils'
 
 # ----------------------------------------------------------------------------------
 
-DATA_MANAGER_CATALYST_METADATA_REPOSITORY_FOLDERPATH = "/Galaxy/DataBank/Catalyst/Data-Manager/Catalyst-Metadata"
-DATA_MANAGER_CATALYST_METADATA_V1_REPOSITORY_FOLDERPATH = "#{DATA_MANAGER_CATALYST_METADATA_REPOSITORY_FOLDERPATH}/metadata-v1"
-$DATA_MANAGER_CATALYST_METADATA_IN_MEMORY_MAP = {}
 =begin
-Map[ObjectUUI, MetadataItem]
 MetadataItem {
     "objectuuid" : UUID
+    "uuid" # copy of objectuuid, used by Iphetra
     (other key value pairs)
 }
 =end
-$DATA_MANAGER_CATALYST_METADATA_IO_SEMAPHORE = Mutex.new
+
+DATA_MANAGER_METADATA_IPHETRA_SETUUID = "abb5af8c-6dd8-466f-8198-c7cca62f8059"
 
 class NSXCatalystMetadataOperator
 
-    # NSXCatalystMetadataOperator::metadataV1FilePaths()
-    def self.metadataV1FilePaths()
-        filepaths = []
-        Find.find(DATA_MANAGER_CATALYST_METADATA_V1_REPOSITORY_FOLDERPATH) do |path|
-            next if !File.file?(path)
-            next if path[-5,5] != ".json"
-            filepaths << path
-        end
-        filepaths
-    end
-
-    # NSXCatalystMetadataOperator::metadataV1InitialLoadFromDisk()
-    def self.metadataV1InitialLoadFromDisk()
-        NSXCatalystMetadataOperator::metadataV1FilePaths()
-            .each{|filepath|
-                begin
-                    metadata = JSON.parse(IO.read(filepath))
-                    $DATA_MANAGER_CATALYST_METADATA_IN_MEMORY_MAP[metadata["objectuuid"]] = metadata
-                rescue
-                end
-            }
-    end
-
     # NSXCatalystMetadataOperator::putItem(metadata)
     def self.putItem(metadata)
-        filename = "#{Digest::SHA1.hexdigest(metadata["objectuuid"])}.json"
-        folderpath = "#{DATA_MANAGER_CATALYST_METADATA_V1_REPOSITORY_FOLDERPATH}/#{filename[0,2]}/#{filename[2,2]}"
-        if !File.exists?(folderpath) then
-            FileUtils.mkpath(folderpath)
-        end
-        File.open("#{folderpath}/#{filename}", "w"){|f| f.puts(JSON.pretty_generate(metadata)) }
-        $DATA_MANAGER_CATALYST_METADATA_IN_MEMORY_MAP[metadata["objectuuid"]] = metadata
+        metadata["uuid"] = metadata["objectuuid"] # for Iphetra
+        Iphetra::commitObjectToDisk(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, DATA_MANAGER_METADATA_IPHETRA_SETUUID, metadata)        
     end
 
     # NSXCatalystMetadataOperator::getMetadataForObject(objectuuid)
@@ -69,7 +39,7 @@ class NSXCatalystMetadataOperator
         newmetadata = {
             "objectuuid" => objectuuid
         }
-        ($DATA_MANAGER_CATALYST_METADATA_IN_MEMORY_MAP[objectuuid] || newmetadata).clone
+        Iphetra::getObjectByUUIDOrNull(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, DATA_MANAGER_METADATA_IPHETRA_SETUUID, objectuuid) || newmetadata
     end
 
     # NSXCatalystMetadataOperator::setMetadataForObject(objectuuid, metadata)
@@ -79,11 +49,8 @@ class NSXCatalystMetadataOperator
 
     # NSXCatalystMetadataOperator::getAllMetadataObjects()
     def self.getAllMetadataObjects()
-        $DATA_MANAGER_CATALYST_METADATA_IN_MEMORY_MAP.values.map{|object| object.clone }
+        Iphetra::getObjects(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, DATA_MANAGER_METADATA_IPHETRA_SETUUID)
     end
 
 end
-
-puts "NSXCatalystMetadataOperator::metadataV1InitialLoadFromDisk()"
-NSXCatalystMetadataOperator::metadataV1InitialLoadFromDisk()
 
