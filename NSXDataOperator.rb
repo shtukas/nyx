54c1dbeb84b53d49ac1a943c39ffd22b7944a084
Pascal Honoré <pascal@alseyn.net>
2018-10-20 23:52:47 +0100

    Misc Updates

--- NSXDataOperator.rb
+++ NSXDataOperator.rb
@@ -52,46 +52,6 @@ end
 
 # ----------------------------------------------------------------------------------
 
-=begin
-MetadataItem {
-    "objectuuid" : UUID
-    "uuid" # copy of objectuuid, used by Iphetra
-    (other key value pairs)
-}
-=end
-
-CATALYST_IPHETRA_METADATA_SETUUID = "abb5af8c-6dd8-466f-8198-c7cca62f8059"
-
-class NSXCatalystMetadataOperator
-
-    # NSXCatalystMetadataOperator::putItem(metadata)
-    def self.putItem(metadata)
-        metadata["uuid"] = metadata["objectuuid"] # for Iphetra
-        Iphetra::commitObjectToDisk(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_METADATA_SETUUID, metadata)        
-    end
-
-    # NSXCatalystMetadataOperator::getMetadataForObject(objectuuid)
-    def self.getMetadataForObject(objectuuid)
-        newmetadata = {
-            "objectuuid" => objectuuid
-        }
-        Iphetra::getObjectByUUIDOrNull(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_METADATA_SETUUID, objectuuid) || newmetadata
-    end
-
-    # NSXCatalystMetadataOperator::setMetadataForObject(objectuuid, metadata)
-    def self.setMetadataForObject(objectuuid, metadata)
-        NSXCatalystMetadataOperator::putItem(metadata)
-    end
-
-    # NSXCatalystMetadataOperator::getAllMetadataObjects()
-    def self.getAllMetadataObjects()
-        Iphetra::getObjects(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_METADATA_SETUUID)
-    end
-
-end
-
-# ----------------------------------------------------------------------------------
-
 $CATALYST_OBJECTS_996CA6AB = {}
 
 class NSXCatalystObjectsOperator
