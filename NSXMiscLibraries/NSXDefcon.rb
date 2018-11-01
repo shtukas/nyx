c3cf23487a8b0c240c6f0091ff55032ff3e97f65
Pascal Honoré <pascal@alseyn.net>
2018-11-01 17:15:52 +0000

    defcon: misc updates

--- NSXMiscLibraries/NSXDefcon.rb
+++ NSXMiscLibraries/NSXDefcon.rb
@@ -19,37 +19,45 @@ Defcon Codes Objects:
 class NSXDefcon
 
     # NSXDefcon::computeObjectDefcon(object)
-    def self.computeObjectDefcon(object) # integer
+    def self.computeObjectDefcon(object) # [integer, string]
         if object["agent-uid"] == "83837e64-554b-4dd0-a478-04386d8010ea" then
             # Baby Nights
-            return 2 # Today important
+            return [2, "37b36b3c"] # Today important
         end
         if ( object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" ) and ( object["schedule"]["@"]=="sticky" ) then
             # Wave, sticky
-            return 1 # Right now
+            return [1, "97f018a8"] # Right now
         end
         if ( object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" ) and ( object["schedule"]["@"]=="every-this-day-of-the-month" ) then
             # Wave, every-this-day-of-the-month
-            return 2 # Today important
+            return [2, "93d81c5b"] # Today important
         end
         if ( object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" ) and ( object["schedule"]["@"]=="every-this-day-of-the-week" ) then
             # Wave, every-this-day-of-the-month
-            return 2 # Today important
+            return [2, "0e7e5620"] # Today important
         end
         if object["agent-uid"] == "f8a8b8e6-623f-4ce1-b6fe-3bc8b34f7a10" then
             # House
-            return 1 # Right now
+            return [1, "34fe2651"] # Right now
         end
-        if object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" then
+        if object["agent-uid"] == "201cac75-9ecc-4cac-8ca1-2643e962a6c6" then
             # LightThreads
-            return 2 # Today important
+            return [2, "a70b49e9"] # Today important
+        end
+        if ( object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" ) and ( object["schedule"]["@"]=="new" ) then
+            # Wave, new
+            return [4, "238f36ac"] # Best efforts
+        end
+        if ( object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d" ) and ( object["schedule"]["@"]=="every-n-hours" ) then
+            # Wave, new
+            return [3, "26ea35fe"] # 3 : Today non important
         end
         0
     end
 
 	# NSXDefcon::computeSystemDefcon(objects)
     def self.computeSystemDefcon(objects) # integer
-    	5
+    	objects.map{|object| NSXDefcon::computeObjectDefcon(object)[0] }.min
     end
 
 	# NSXDefcon::defconSelection(objects)
@@ -58,7 +66,9 @@ class NSXDefcon
         # We compute the system defcon, and
         # We display the objects of the right defcon.
         objects = objects.map{|object| 
-            object[":defcon:"] = NSXDefcon::computeObjectDefcon(object) 
+            defcon, defconOrigin = NSXDefcon::computeObjectDefcon(object)
+            object[":defcon:"] = defcon
+            object[":defcon-origin:"] = defconOrigin
             object
         }
     	defcon = NSXDefcon::computeSystemDefcon(objects)
