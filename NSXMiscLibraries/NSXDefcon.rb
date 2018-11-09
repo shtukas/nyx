8e40cc00375e9316a173521ca61cc4b9dcc656bc
Pascal Honoré <pascal@alseyn.net>
2018-11-09 22:16:10 +0000

    defcon updates

--- NSXMiscLibraries/NSXDefcon.rb
+++ NSXMiscLibraries/NSXDefcon.rb
@@ -37,17 +37,13 @@ class NSXDefcon
             # Baby Nights
             return [2, "37b36b3c"] # Today important
         end
-        if (object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d") and (object["schedule"]["@"]=="new") and object["announce"].include?("[new] http") then
+        if (object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d") and object["announce"].include?("email") then
             # Wave, new, http # mostly former stream
-            return [3, "201b26f8"] # 3 : Today non important
-        end
-        if (object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d") and (object["schedule"]["@"]=="new") and object["announce"].include?("new") then
-            # Wave, new
-            return [1, "e7a13247"] # 1 : Right now
+            return [1, "201b26f8"] # 1 : Right now
         end
         if (object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d") and (object["schedule"]["@"]=="new") then
             # Wave, new
-            return [4, "238f36ac"] # Best efforts
+            return [3, "238f36ac"] # 3 : Today non important
         end
         if (object["agent-uid"] == "283d34dd-c871-4a55-8610-31e7c762fb0d") and (object["schedule"]["@"]=="sticky") then
             # Wave, sticky
