14ba75985c2f262a7060268996e29b6e2e204ca0
Pascal Honoré <pascal@alseyn.net>
2018-11-30 10:58:29 +0000

    Decommissioning NSXSystemDataKeyValueStore

--- NSXMiscLibraries/NSXKeyValueStores.rb
+++ NSXMiscLibraries/NSXKeyValueStores.rb
@@ -6,49 +6,6 @@
 
 # ----------------------------------------------------------------------------------
 
-CATALYST_IPHETRA_SYSTEM_DATA_SETUUID = "e13183f1-4615-49a9-8862-b23a38783f26"
-
-=begin
-{
-    "uuid"  => key,
-    "value" => value
-}
-=end
-
-class NSXSystemDataKeyValueStore
-
-    # NSXSystemDataKeyValueStore::set(key, value)
-    def self.set(key, value)
-        object = {
-            "uuid"  => key,
-            "value" => value
-        }
-        Iphetra::commitObjectToDisk(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_SYSTEM_DATA_SETUUID, object)
-    end
-
-    # NSXSystemDataKeyValueStore::getOrNull(key)
-    def self.getOrNull(key)
-        object = Iphetra::getObjectByUUIDOrNull(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_SYSTEM_DATA_SETUUID, key)
-        return nil if object.nil?
-        object["value"]
-    end
-
-    # NSXSystemDataKeyValueStore::getOrDefaultValue(key, defaultValue)
-    def self.getOrDefaultValue(key, defaultValue)
-        value = NSXSystemDataKeyValueStore::getOrNull(key)
-        return value if value
-        defaultValue
-    end
-
-    # NSXSystemDataKeyValueStore::destroy(key)
-    def self.destroy(key)
-        Iphetra::destroyObject(CATALYST_IPHETRA_DATA_REPOSITORY_FOLDERPATH, CATALYST_IPHETRA_SYSTEM_DATA_SETUUID, key)
-    end
-
-end
-
-# ----------------------------------------------------------------------------------
-
 CATALYST_IPHETRA_AGENT_DATA_SETUUID_PREFIX = "d0b1f843-324d-469e-80e6-b0f330491287"
 
 =begin
