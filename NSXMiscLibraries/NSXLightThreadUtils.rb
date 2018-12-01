2bc04c8ff07b44a89b86caa1ce11ebccff111026
Pascal Honoré <pascal@alseyn.net>
2018-12-01 19:20:25 +0000

    Refactoring

--- NSXMiscLibraries/NSXLightThreadUtils.rb
+++ NSXMiscLibraries/NSXLightThreadUtils.rb
@@ -397,20 +397,6 @@ class NSXLightThreadMetrics
 
 end
 
-$lightThreadToItsStreamItemsOrderedCache = {}                             # marker: 73ca550c-9508
-$lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap = {} # marker: 73ca550c-9508
-
-def nsxLightThreadsStreamsItemsOrdered_getCacheKey(lightThreadUUID) # marker: 73ca550c-9508
-    if $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID].nil? then
-        $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID] = SecureRandom.hex
-    end
-    $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID]
-end
-
-def nsxLightThreadsStreamsItemsOrdered_resetCacheKey(lightThreadUUID) # marker: 73ca550c-9508
-    $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap.delete(lightThreadUUID)
-end
-
 class NSXLightThreadsStreamsInterface
 
     # NSXLightThreadsStreamsInterface::lightThreadToCompanionFilepath(lightThread)
@@ -464,20 +450,9 @@ class NSXLightThreadsStreamsInterface
 
     # NSXLightThreadsStreamsInterface::lightThreadToItsStreamItemsOrdered(lightThread)
     def self.lightThreadToItsStreamItemsOrdered(lightThread)
-
-        cacheKey = nsxLightThreadsStreamsItemsOrdered_getCacheKey(lightThread["uuid"]) # marker: 73ca550c-9508
-        if $lightThreadToItsStreamItemsOrderedCache[cacheKey] then                     # marker: 73ca550c-9508
-            return $lightThreadToItsStreamItemsOrderedCache[cacheKey]                  # marker: 73ca550c-9508
-        end                                                                            # marker: 73ca550c-9508
-
-        items = NSXStreamsUtils::allStreamsItemsEnumerator()
+        NSXStreamsUtils::allStreamsItemsEnumerator()
             .select{|item| item["streamuuid"]==lightThread["streamuuid"] }
             .sort{|i1, i2| i1["ordinal"]<=>i2["ordinal"] }
-
-        $lightThreadToItsStreamItemsOrderedCache[cacheKey] = items # marker: 73ca550c-9508
-
-        items
-
     end
 
     # NSXLightThreadsStreamsInterface::filterAwayStreamItemsThatAreDoNotShowUntilHidden(items)
@@ -489,3 +464,42 @@ class NSXLightThreadsStreamsInterface
     end
 
 end
+
+# -----------------------------------------------------------------------
+# Cache System
+# Speeding up NSXLightThreadsStreamsInterface::lightThreadToItsStreamItemsOrdered
+# -----------------------------------------------------------------------
+
+$lightThreadToItsStreamItemsOrderedCache = {} # marker: 73ca550c-9508
+$lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap = {} # marker: 73ca550c-9508
+
+def nsxLightThreadsStreamsItemsOrdered_getCacheKey(lightThreadUUID) # marker: 73ca550c-9508
+    if $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID].nil? then
+        $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID] = SecureRandom.hex
+    end
+    $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap[lightThreadUUID]
+end
+
+def nsxLightThreadsStreamsItemsOrdered_resetCacheKey(lightThreadUUID) # marker: 73ca550c-9508
+    $lightThreadToItsStreamItemsOrderedCacheLightThreadUUIDToCacheKeyMap.delete(lightThreadUUID)
+end
+
+class NSXLightThreadsStreamsInterface
+
+    # NSXLightThreadsStreamsInterface::lightThreadToItsStreamItemsOrdered(lightThread)
+    def self.lightThreadToItsStreamItemsOrdered(lightThread) # marker: 73ca550c-9508
+        cacheKey = nsxLightThreadsStreamsItemsOrdered_getCacheKey(lightThread["uuid"]) 
+        if $lightThreadToItsStreamItemsOrderedCache[cacheKey] then
+            return $lightThreadToItsStreamItemsOrderedCache[cacheKey]
+        end
+        items = NSXStreamsUtils::allStreamsItemsEnumerator()
+            .select{|item| item["streamuuid"]==lightThread["streamuuid"] }
+            .sort{|i1, i2| i1["ordinal"]<=>i2["ordinal"] }
+        $lightThreadToItsStreamItemsOrderedCache[cacheKey] = items
+        items
+    end
+
+end
+
+
+
