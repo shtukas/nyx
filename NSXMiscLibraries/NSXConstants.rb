2f26d759c982c4306a30b06ca8ffbcf595c6f023
Pascal Honoré <pascal@alseyn.net>
2018-12-14 15:53:49 +0000

    ... was a bad idea 😬

--- NSXMiscLibraries/NSXConstants.rb
+++ NSXMiscLibraries/NSXConstants.rb
@@ -1,18 +1,5 @@
 
 # encoding: UTF-8
 
-def catalyst_folderpath_2d2cda3e()
-    if ENV["COMPUTERLUCILLENAME"] == "Lucille18" then
-        "/Galaxy/DataBank/Catalyst"
-    else
-        remotePath = "/Volumes/Lucille18/Galaxy/DataBank/Catalyst"
-        if File.exists?(remotePath) then
-            remotePath
-        else
-            raise "Cannot see #{remotePath}"
-        end
-    end
-end
-
-CATALYST_COMMON_DATABANK_CATALYST_FOLDERPATH = catalyst_folderpath_2d2cda3e()
+CATALYST_COMMON_DATABANK_CATALYST_FOLDERPATH = "/Galaxy/DataBank/Catalyst"
 CATALYST_COMMON_BIN_ARCHIVES_TIMELINE_FOLDERPATH = "#{CATALYST_COMMON_DATABANK_CATALYST_FOLDERPATH}/Bin-Timeline"
