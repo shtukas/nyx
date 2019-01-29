216b624544fcf92b6aa38bcd941178b87455e2c5
Pascal Honoré <pascal@alseyn.net>
2019-01-29 18:54:32 +0000

    Ninja updates

--- NSXAgents/NSXAgentNinja.rb
+++ NSXAgents/NSXAgentNinja.rb
@@ -14,28 +14,28 @@ class NSXAgentNinja
         "d3d1d26e-68b5-4a99-a372-db8eb6c5ba58"
     end
 
-    # NSXAgentNinja::getImpactsForDisk()
-    def self.getImpactsForDisk()
+    # NSXAgentNinja::getImpactsFromDisk()
+    def self.getImpactsFromDisk()
         JSON.parse(IO.read("/Galaxy/DataBank/Catalyst/Agents-Data/Ninja/impacts.json"))
     end
 
     # NSXAgentNinja::issueImpact()
     def self.issueImpact()
-        impacts = NSXAgentNinja::getImpactsForDisk()
+        impacts = NSXAgentNinja::getImpactsFromDisk()
         impacts << Time.new.to_i
         File.open("/Galaxy/DataBank/Catalyst/Agents-Data/Ninja/impacts.json", "w"){|f| f.puts(JSON.generate(impacts)) }
     end
 
     # NSXAgentNinja::impactMetricCoefficient()
     def self.impactMetricCoefficient()
-        impacts = NSXAgentNinja::getImpactsForDisk().reject{|impact| (Time.new.to_i - impact) > 86400 }
+        impacts = NSXAgentNinja::getImpactsFromDisk().reject{|impact| (Time.new.to_i - impact) > 86400 }
         return 1 if impacts.size==0
         1 - impacts.map{|unixtime| 0.1*Math.exp(-(Time.new.to_i - unixtime).to_f/3600) }.inject(0, :+)
     end
 
     # NSXAgentNinja::impactsGarbageCollection()
     def self.impactsGarbageCollection()
-        impacts = NSXAgentNinja::getImpactsForDisk().reject{|impact| (Time.new.to_i - impact) > 86400 }
+        impacts = NSXAgentNinja::getImpactsFromDisk().reject{|impact| (Time.new.to_i - impact) > 86400 }
         File.open("/Galaxy/DataBank/Catalyst/Agents-Data/Ninja/impacts.json", "w"){|f| f.puts(JSON.generate(impacts)) }
     end
 
