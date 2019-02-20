2c1c53c9b2a7cdbaa5db90a3b8842e4929b5cadb
Pascal Honoré <pascal@alseyn.net>
2019-02-20 23:10:31 +0000

    Some evening fun

--- NSXMiscLibraries/NSXCatalystObjectsOperator.rb
+++ NSXMiscLibraries/NSXCatalystObjectsOperator.rb
@@ -28,14 +28,44 @@ class NSXCatalystObjectsOperator
         NSXCatalystObjectsOperator::getObjects().select{|object| objectuuids.include?(object["uuid"]) }
     end
 
-    # NSXCatalystObjectsOperator::catalystObjectsForMainListing()
-    def self.catalystObjectsForMainListing()
+    # NSXCatalystObjectsOperator::getAliveObjects()
+    def self.getAliveObjects()
         objects = NSXCatalystObjectsOperator::getObjects()
             .map{|object| object["isRunning"] ? object : NSXMiscUtils::fDoNotShowUntilDateTimeUpdateForDisplay(object) }
             .select{|object| object["metric"] >= 0.2 }
-        objects = objects    
-            .sort{|o1, o2| o1["metric"]<=>o2["metric"] }
-            .reverse
+    end
+
+    # NSXCatalystObjectsOperator::getStoredFrozenObjectsOrNull()
+    def self.getStoredFrozenObjectsOrNull()
+        return nil if Time.new.hour<18
+        objects = KeyValueStore::getOrNull(nil, "d685c96f-250f-4529-ba85-eb7f38a18b39")
+        return nil if objects.nil?
+        JSON.parse(objects)
+    end
+
+    # NSXCatalystObjectsOperator::getManagedFrozenObjectsOrNull()
+    def self.getManagedFrozenObjectsOrNull()
+        aliveObjects = NSXCatalystObjectsOperator::getAliveObjects()
+        frozenObjects = NSXCatalystObjectsOperator::getStoredFrozenObjectsOrNull()
+        if frozenObjects.nil? then
+            aliveObjectsSample = aliveObjects.sample(10).shuffle
+            if aliveObjectsSample.size<5 then
+                return nil
+            end
+            KeyValueStore::set(nil, "d685c96f-250f-4529-ba85-eb7f38a18b39", JSON.generate(aliveObjectsSample))
+            return aliveObjectsSample
+        else
+            frozenObjectsStillCurrent = frozenObjects.select{|fobject| aliveObjects.map{|o| o["uuid"] }.include?(fobject["uuid"]) }
+            if frozenObjectsStillCurrent.size<5 then
+                KeyValueStore::destroy(nil, "d685c96f-250f-4529-ba85-eb7f38a18b39")
+                return NSXCatalystObjectsOperator::getManagedFrozenObjectsOrNull()
+            end
+            return frozenObjectsStillCurrent
+        end     
+    end
+
+    # NSXCatalystObjectsOperator::aliveObjectsSpecialCircumstancesProcessing(object)
+    def self.aliveObjectsSpecialCircumstancesProcessing(object)
         minusEmailsUnixtime = NSXMiscUtils::getMinusEmailsUnixtimeOrNull()
         if minusEmailsUnixtime and (Time.new.to_i - minusEmailsUnixtime) < 3600 then
             objects = objects.reject{|object|
@@ -43,6 +73,18 @@ class NSXCatalystObjectsOperator
             }
         end
         objects
+    end
+
+    # NSXCatalystObjectsOperator::catalystObjectsForMainListing()
+    def self.catalystObjectsForMainListing()
+        if Time.new.hour>=18 then
+            objects = NSXCatalystObjectsOperator::getManagedFrozenObjectsOrNull()
+            return objects if objects
+        end
+        objects = NSXCatalystObjectsOperator::getAliveObjects()   
+            .sort{|o1, o2| o1["metric"]<=>o2["metric"] }
+            .reverse
+        NSXCatalystObjectsOperator::aliveObjectsSpecialCircumstancesProcessing(object)
     end
 
 end
