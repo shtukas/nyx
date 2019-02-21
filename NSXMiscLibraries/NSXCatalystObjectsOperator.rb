0236949aad88627fa4e7820e92fbab704bf3d6de
Pascal Honoré <pascal@alseyn.net>
2019-02-21 12:20:30 +0000

    NSXCatalystObjectsOperator::aliveObjectsSpecialCircumstancesProcessing (update)

--- NSXMiscLibraries/NSXCatalystObjectsOperator.rb
+++ NSXMiscLibraries/NSXCatalystObjectsOperator.rb
@@ -21,12 +21,46 @@ class NSXCatalystObjectsOperator
 
     # NSXCatalystObjectsOperator::aliveObjectsSpecialCircumstancesProcessing(objects)
     def self.aliveObjectsSpecialCircumstancesProcessing(objects)
+
+        # ----------------------------------------------------
+        # Do not display emails when emails are off
         minusEmailsUnixtime = NSXMiscUtils::getMinusEmailsUnixtimeOrNull()
         if minusEmailsUnixtime and (Time.new.to_i - minusEmailsUnixtime) < 3600 then
             objects = objects.reject{|object|
                 (object["agentuid"]=="d2de3f8e-6cf2-46f6-b122-58b60b2a96f1") and object["data"]["generic-contents-item"]["type"] == "email"
             }
         end
+
+        # ----------------------------------------------------
+        # Apply special mapping for less then 0.5 elements
+        getStoredUUIDsOfLowElementsOrNull = lambda {
+            uuids = KeyValueStore::getOrNull(nil, "3714dfd8-6bbb-413e-a9ef-4022508ce4e4:#{NSXMiscUtils::currentDay()}")
+            return nil if uuids.nil?
+            JSON.parse(uuids)
+        }
+        getTodayUUIDsOfLowElements = lambda {|objects|
+            uuids = getStoredUUIDsOfLowElementsOrNull.call()
+            return uuids if uuids
+            uuids = objects
+                        .select{|object| object["metric"] < 0.5 }
+                        .map{|object| object["uuid"] }
+            KeyValueStore::set(nil, "3714dfd8-6bbb-413e-a9ef-4022508ce4e4:#{NSXMiscUtils::currentDay()}", JSON.generate(uuids))
+            uuids
+        }
+        metricRecomputation = lambda {|metric|
+            # [0.2, 0.6] -> [0.6, 0.9]
+            ((metric-0.2).to_f/4)*3 + 0.6
+        }
+        uuidsx = getTodayUUIDsOfLowElements.call(objects)
+        objects = objects.map{|object|
+            if uuidsx.include?(object["uuid"]) then
+                object["metric-pre-bump:61a9ee39"] = object["metric"]
+                object["metric"] = metricRecomputation.call(object["metric"])
+            end
+            object
+        }
+
+        # ----------------------------------------------------
         objects
     end
 
