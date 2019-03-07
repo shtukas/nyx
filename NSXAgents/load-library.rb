aea5c46e6fa69b9d179f630533b5c0038b35757d
Pascal Honoré <pascal@alseyn.net>
2019-03-07 09:06:59 +0000

    Better library loading

---
+++ NSXAgents/load-library.rb
@@ -0,0 +1,11 @@
+
+# encoding: UTF-8
+
+require_relative "NSXAgentBabyNights.rb"
+require_relative "NSXAgentTodayNotes.rb"
+require_relative "NSXAgentHouse.rb"
+require_relative "NSXAgentLightThread.rb"
+require_relative "NSXAgentStreams.rb"
+require_relative "NSXAgentTheBridge.rb"
+require_relative "NSXAgentVienna.rb"
+require_relative "NSXAgentWave.rb"
---
+++ NSXMiscLibraries/load-library.rb
@@ -0,0 +1,21 @@
+
+# encoding: UTF-8
+
+require_relative "NSXCatalystUI.rb"
+require_relative "NSXConstants.rb"
+require_relative "NSXBob.rb"
+require_relative "NSXCatalystObjectsOperator.rb"
+require_relative "NSXData.rb"
+require_relative "NSXDisplayUtils.rb"
+require_relative "NSXGeneralCommandHandler.rb"
+require_relative "NSXGenericContents.rb"
+require_relative "NSXDoNotShowUntilDatetime.rb"
+require_relative "NSXEstateServices.rb"
+require_relative "NSXEmailClients.rb"
+require_relative "NSXEmailTrackingClaims.rb"
+require_relative "NSXFolderProbe.rb"
+require_relative "NSXLightThreadUtils.rb"
+require_relative "NSXMiscUtils.rb"
+require_relative "NSXRunner.rb"
+require_relative "NSXStreamsUtils.rb"
+require_relative "NSXStringParser.rb"
