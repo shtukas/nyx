61f1a525f758592e664b77e925ea0b4a016f72d5
Pascal Honoré <pascal@alseyn.net>
2019-03-24 13:49:05 +0000

    NSXPlacement update

--- NSXMiscLibraries/NSXPlacement.rb
+++ NSXMiscLibraries/NSXPlacement.rb
@@ -16,20 +16,24 @@ class NSXPlacement
 
     # NSXPlacement::getValue(objectuuid)
     def self.getValue(objectuuid)
-        key = "6d232da2-4bc3-485b-8b78-d9a4d372d587:#{objectuuid}"
+        key = "6d232da2-4bc3-485b-8b78-d9a4d372d589:#{NSXMiscUtils::currentDay()}:#{objectuuid}"
         value = KeyValueStore::getOrNull(nil, key)
         if value then
             value.to_i
         else
             value = $NSXPlacementX11.values.max + 1
+            puts "placement: #{objectuuid} @ #{value}"
+            $NSXPlacementX11[objectuuid] = value
             KeyValueStore::set(nil, key, value)
             value
         end
     end
 
+    # NSXPlacement::rotate(objectuuid)
     def self.rotate(objectuuid)
-        key = "6d232da2-4bc3-485b-8b78-d9a4d372d587:#{objectuuid}"
+        key = "6d232da2-4bc3-485b-8b78-d9a4d372d589:#{NSXMiscUtils::currentDay()}:#{objectuuid}"
         value = $NSXPlacementX11.values.max + 1
+        $NSXPlacementX11[objectuuid] = value
         KeyValueStore::set(nil, key, value)
     end
 end
