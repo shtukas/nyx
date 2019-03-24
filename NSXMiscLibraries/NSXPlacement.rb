e774d0959e662df0bcdd97d4a8c37db47206f558
Pascal Honoré <pascal@alseyn.net>
2019-03-24 13:04:22 +0000

    Introduce NSXPlacement

---
+++ NSXMiscLibraries/NSXPlacement.rb
@@ -0,0 +1,35 @@
+
+# encoding: UTF-8
+
+require "/Galaxy/Software/Misc-Common/Ruby-Libraries/KeyValueStore.rb"
+=begin
+    KeyValueStore::set(repositorylocation or nil, key, value)
+    KeyValueStore::getOrNull(repositorylocation or nil, key)
+    KeyValueStore::getOrDefaultValue(repositorylocation or nil, key, defaultValue)
+    KeyValueStore::destroy(repositorylocation or nil, key)
+=end
+
+$NSXPlacementX11 = {}
+$NSXPlacementX11[""] = 0
+
+class NSXPlacement
+
+    # NSXPlacement::getValue(objectuuid)
+    def self.getValue(objectuuid)
+        key = "6d232da2-4bc3-485b-8b78-d9a4d372d587:#{objectuuid}"
+        value = KeyValueStore::getOrNull(nil, key)
+        if value then
+            value.to_i
+        else
+            value = $NSXPlacementX11.values.max + 1
+            KeyValueStore::set(nil, key, value)
+            value
+        end
+    end
+
+    def self.rotate(objectuuid)
+        key = "6d232da2-4bc3-485b-8b78-d9a4d372d587:#{objectuuid}"
+        value = $NSXPlacementX11.values.max + 1
+        KeyValueStore::set(nil, key, value)
+    end
+end
