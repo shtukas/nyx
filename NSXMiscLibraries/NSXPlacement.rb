7a64d717365398f756dac43fdf9a4a7a4453806b
Pascal Honoré <pascal@alseyn.net>
2019-03-31 07:54:09 +0100

    Update in NSXPlacement (values per day)

--- NSXMiscLibraries/NSXPlacement.rb
+++ NSXMiscLibraries/NSXPlacement.rb
@@ -10,36 +10,50 @@ require "/Galaxy/Software/Misc-Common/Ruby-Libraries/KeyValueStore.rb"
 =end
 
 $NSXPlacementX11 = JSON.parse(
-    KeyValueStore::getOrDefaultValue(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d589:#{Time.now.utc.iso8601[0,10]}", "{}")
+    KeyValueStore::getOrDefaultValue(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d599:#{Time.now.utc.iso8601[0,10]}", "{}")
 )
 
 class NSXPlacement
 
+    # NSXPlacement::currentDate()
+    def self.currentDate()
+        Time.now.utc.iso8601[0,10]
+    end
+
     # NSXPlacement::getValue(objectuuid)
     def self.getValue(objectuuid)
-        value = $NSXPlacementX11[objectuuid]
+        if $NSXPlacementX11[NSXPlacement::currentDate()].nil? then
+            $NSXPlacementX11[NSXPlacement::currentDate()] = {}
+        end
+        value = $NSXPlacementX11[NSXPlacement::currentDate()][objectuuid]
         if value then
             value
         else
-            value = ($NSXPlacementX11.values + [0]).max + 1
+            value = ($NSXPlacementX11[NSXPlacement::currentDate()].values + [0]).max + 1
             puts "placement: #{objectuuid} @ #{value}"
-            $NSXPlacementX11[objectuuid] = value
-            KeyValueStore::set(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d589:#{Time.now.utc.iso8601[0,10]}", JSON.generate($NSXPlacementX11))
+            $NSXPlacementX11[NSXPlacement::currentDate()][objectuuid] = value
+            KeyValueStore::set(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d599:#{NSXPlacement::currentDate()}", JSON.generate($NSXPlacementX11))
             value
         end
     end
 
     # NSXPlacement::rotate(objectuuid)
     def self.rotate(objectuuid)
-        value = ($NSXPlacementX11.values + [0]).max + 1
-        $NSXPlacementX11[objectuuid] = value
-        KeyValueStore::set(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d589:#{Time.now.utc.iso8601[0,10]}", JSON.generate($NSXPlacementX11))
+        if $NSXPlacementX11[NSXPlacement::currentDate()].nil? then
+            $NSXPlacementX11[NSXPlacement::currentDate()] = {}
+        end
+        value = ($NSXPlacementX11[NSXPlacement::currentDate()].values + [0]).max + 1
+        $NSXPlacementX11[NSXPlacement::currentDate()][objectuuid] = value
+        KeyValueStore::set(nil, "6d232da2-4bc3-485b-8b78-d9b4d372d599:#{NSXPlacement::currentDate()}", JSON.generate($NSXPlacementX11))
     end
 
     # NSXPlacement::clean(objectuuids)
     def self.clean(objectuuids)
-        ($NSXPlacementX11.keys - objectuuids).each{|objectuuid|
-            $NSXPlacementX11.delete(objectuuid)
+        if $NSXPlacementX11[NSXPlacement::currentDate()].nil? then
+            $NSXPlacementX11[NSXPlacement::currentDate()] = {}
+        end
+        ($NSXPlacementX11[NSXPlacement::currentDate()].keys - objectuuids).each{|objectuuid|
+            $NSXPlacementX11[NSXPlacement::currentDate()].delete(objectuuid)
         }
     end
 
