58e30186015d4202572d224c1d089b852529dc01
Pascal Honoré <pascal@alseyn.net>
2019-09-02 18:22:18 +0100

    Corrections in NSXAgentDailyGuardianWork

--- NSXAgents/NSXAgentDailyGuardianWork.rb
+++ NSXAgents/NSXAgentDailyGuardianWork.rb
@@ -36,21 +36,20 @@ class NSXAgentDailyGuardianWork
         return [] if KeyValueStore::flagIsTrue(nil, "33319c02-f1cd-4296-a772-43bb5b6ba07f:#{NSXMiscUtils::currentDay()}")
         object = {}
         object["uuid"] = "392eb09c-572b-481d-9e8e-894e9fa016d4-so1"
-        object["agentuid"] = nil
+        object["agentuid"] = "a6d554fd-44bf-4937-8dc6-5c9f1dcdaeba"
         object["metric"] = 0.60
         object["announce"] = "Daily Guardian Work"
         object["commands"] = ["done"]
-        object["executionLambdas"] = {
-            "done" => lambda{|object| 
-                KeyValueStore::setFlagTrue(nil, "33319c02-f1cd-4296-a772-43bb5b6ba07f:#{NSXMiscUtils::currentDay()}")
-            }
-        }
         [object]
     end
 
     # NSXAgentDailyGuardianWork::processObjectAndCommand(objectuuid, command, isLocalCommand)
     def self.processObjectAndCommand(objectuuid, command, isLocalCommand)
-        if command == "open" then
+        if command == "done" then
+            KeyValueStore::setFlagTrue(nil, "33319c02-f1cd-4296-a772-43bb5b6ba07f:#{NSXMiscUtils::currentDay()}")
+            if isLocalCommand then
+                NSXMultiInstancesWrite::issueEventCommand(objectuuid, NSXAgentDailyGuardianWork::agentuid(), "done")
+            end
             return 
         end
     end
