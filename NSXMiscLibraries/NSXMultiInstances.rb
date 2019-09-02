5b1f5717eb724c0a66ecddeb7ff47fd2132fc933
Pascal Honoré <pascal@alseyn.net>
2019-09-02 21:00:08 +0100

    Activity-Log

--- NSXMiscLibraries/NSXMultiInstances.rb
+++ NSXMiscLibraries/NSXMultiInstances.rb
@@ -10,7 +10,7 @@ require 'fileutils'
 
 require "/Galaxy/Software/Misc-Common/Ruby-Libraries/LucilleCore.rb"
 
-MULTIINSTANCE_LOG_FOLDERPATH = "#{CATALYST_COMMON_DATABANK_CATALYST_SHARED_FOLDERPATH}/Multi-Instance-Log"
+MULTIINSTANCE_LOG_FOLDERPATH = "#{CATALYST_COMMON_DATABANK_CATALYST_SHARED_FOLDERPATH}/Activity-Log"
 
 class NSXMultiInstancesWrite
 
