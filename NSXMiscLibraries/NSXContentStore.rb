cf004c99417a3b9f22d08f8f3bf7dcaf8d0f3029
Pascal Honoré <pascal@alseyn.net>
2019-09-02 20:40:16 +0100

    Catalyst-Singleton

---
+++ NSXMiscLibraries/NSXContentStore.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+# encoding: UTF-8
+
+require "/Galaxy/Software/Misc-Common/Ruby-Libraries/BTreeSets.rb"
+=begin
+    BTreeSets::values(repositorylocation or nil, setuuid: String): Array[Value]
+    BTreeSets::set(repositorylocation or nil, setuuid: String, valueuuid: String, value)
+    BTreeSets::getOrNull(repositorylocation or nil, setuuid: String, valueuuid: String): nil | Value
+    BTreeSets::destroy(repositorylocation, setuuid: String, valueuuid: String)
+=end
+
+require 'securerandom'
+# SecureRandom.hex    #=> "eb693ec8252cd630102fd0d0fb7c3485"
+# SecureRandom.hex(4) #=> "eb693123"
+# SecureRandom.uuid   #=> "2d931510-d99f-494a-8c67-87feb05e1594"
+
+=begin
+
+=end
+
+class NSXContentStore
+
+    # ContentStore::getItemOrNull(contentStoreItemId: String): Item or null
+    def self.getItemOrNull(contentStoreItemId)
+
+    end
+
+end
