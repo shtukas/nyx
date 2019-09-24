c9dc54ea91c4692b1b7c66adfb96b5eadd530be1
Pascal Honoré <pascal@alseyn.net>
2019-09-24 18:10:25 +0100

    Updates in Scala/LucilleFile

--- Catalyst-Scala/src/main/scala/catalyst/LucilleFile.scala
+++ Catalyst-Scala/src/main/scala/catalyst/LucilleFile.scala
@@ -1,10 +1,16 @@
 package catalyst
 
+import scala.io.Source
+
 object LucilleFile {
     def getLines(): Array[String] = {
-        Array(
-            "Buy some bread"
-        )
+        Source.fromFile("/Users/pascal/Desktop/Lucille18.txt" ,"UTF-8")
+          .mkString
+          .split("@marker-539d469a-8521-4460-9bc4-5fb65da3cd4b")
+          .head
+          .lines
+          .toArray
+          .filter(str => str.length > 0)
     }
     def printLines(): Unit = {
         getLines().foreach{line => println(line)}
