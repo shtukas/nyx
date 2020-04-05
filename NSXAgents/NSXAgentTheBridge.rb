2cba9f8686c5beb92d2800a0929058450536d590
Pascal Honoré <pascal@alseyn.net>
2020-04-05 14:39:43 +0100

    NSXAgentTheBridge update

--- NSXAgents/NSXAgentTheBridge.rb
+++ NSXAgents/NSXAgentTheBridge.rb
@@ -1,12 +1,16 @@
 #!/usr/bin/ruby
 
 # encoding: UTF-8
-require "/Users/pascal/Galaxy/LucilleOS/Software-Common/Ruby-Libraries/LucilleCore.rb"
+
+
 require 'securerandom'
 # SecureRandom.hex    #=> "eb693ec8252cd630102fd0d0fb7c3485"
 # SecureRandom.hex(4) #=> "eb693123"
 # SecureRandom.uuid   #=> "2d931510-d99f-494a-8c67-87feb05e1594"
+
 require "time"
+
+require "/Users/pascal/Galaxy/LucilleOS/Software-Common/Ruby-Libraries/LucilleCore.rb"
 
 # -------------------------------------------------------------------------------------
 
@@ -31,7 +35,22 @@ class NSXAgentTheBridge
     def self.getAllObjects()
         NSXAgentTheBridge::sources()
             .map{|source|
-                JSON.parse(`#{source}`)
+                begin
+                    JSON.parse(`#{source}`)
+                rescue
+                    [
+                        {
+                            "uuid"            => SecureRandom.hex,
+                            "agentuid"        => nil,
+                            "contentItem"     => {
+                                "type" => "line",
+                                "line" => "Problems extracting catalyst objects at '#{source}'"
+                            },
+                            "metric"          => 1,
+                            "commands"        => []
+                        }
+                    ]
+                end
             }
             .flatten
     end
