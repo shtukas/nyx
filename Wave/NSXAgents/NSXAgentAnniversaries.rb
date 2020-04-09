ffa2384e9ecbb29edac2a7e2d4e73adc99e4542a
Pascal Honoré <pascal@alseyn.net>
2020-04-09 21:06:03 +0100

    File system update (9)

--- Wave/NSXAgents/NSXAgentAnniversaries.rb
+++ Wave/NSXAgents/NSXAgentAnniversaries.rb
@@ -70,7 +70,7 @@ class NSXAgentAnniversaries
     end
 
     def self.getEventLines()
-        IO.read("#{DATABANK_FOLDER_PATH}/anniversaries/anniversaries.txt")
+        IO.read("#{CATALYST_FOLDERPATH}/Wave/anniversaries/anniversaries.txt")
             .lines
             .map{|line| line.strip }
             .select{|line| line.size > 0 }
@@ -114,7 +114,7 @@ class NSXAgentAnniversaries
     end
 
     def self.trueIfAnniversaryHasBeenProcessed(event, anniversary)
-        KeyValueStore::flagIsTrue("#{DATABANK_FOLDER_PATH}/anniversaries/kvstore-data", "b05b9dae-93d5-40f0-b68c-8cec95804b89:#{event["description"]}:#{JSON.generate(anniversary)}")
+        KeyValueStore::flagIsTrue("#{CATALYST_FOLDERPATH}/Wave/anniversaries/kvstore-data", "b05b9dae-93d5-40f0-b68c-8cec95804b89:#{event["description"]}:#{JSON.generate(anniversary)}")
     end
 
     def self.getEventObjects()
@@ -150,7 +150,7 @@ class NSXAgentAnniversaries
     end
 
     def self.markAnniversaryAsProcessed(event, anniversary)
-        KeyValueStore::setFlagTrue("#{DATABANK_FOLDER_PATH}/anniversaries/kvstore-data", "b05b9dae-93d5-40f0-b68c-8cec95804b89:#{event["description"]}:#{JSON.generate(anniversary)}")
+        KeyValueStore::setFlagTrue("#{CATALYST_FOLDERPATH}/Wave/anniversaries/kvstore-data", "b05b9dae-93d5-40f0-b68c-8cec95804b89:#{event["description"]}:#{JSON.generate(anniversary)}")
     end
 
     # NSXAgentAnniversaries::getAllObjects()
