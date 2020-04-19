6f3a86d7c8f189d41b1f1b23680e695ff8789293
Pascal Honoré <pascal@alseyn.net>
2020-04-19 01:19:46 +0100

    Lucille upgrades (5)

--- Lucille/Lucille.rb
+++ Lucille/Lucille.rb
@@ -197,7 +197,7 @@ class Lucille
         return if !File.exists?(location)
         folder1 = "#{BIN_TIMELINE_FOLDERPATH}/#{Time.new.strftime("%Y")}/#{Time.new.strftime("%Y-%m-%d")}/#{Time.new.strftime("%Y-%m-%d")}"
         folder2 = LucilleCore::indexsubfolderpath(folder1)
-        folder3 = "#{folder2}/#{NSXMiscUtils::timeStringL22()}"
+        folder3 = "#{folder2}/#{Lucille::timeStringL22()}"
         FileUtils.mkdir(folder3)
         FileUtils.mv(location, folder3)
     end
