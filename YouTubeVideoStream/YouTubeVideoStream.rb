687e4de439d9884737544303cb3848e3314f8f56
Pascal Honoré <pascal@alseyn.net>
2020-04-28 23:20:08 +0100

    Metric updates

--- YouTubeVideoStream/YouTubeVideoStream.rb
+++ YouTubeVideoStream/YouTubeVideoStream.rb
@@ -36,23 +36,6 @@ class YouTubeVideoStream
         "/Volumes/EnergyGrid/Data/Pascal/Galaxy/YouTube Videos"
     end
 
-    # YouTubeVideoStream::registerHit()
-    def self.registerHit()
-        points = KeyValueStore::getOrDefaultValue(nil, "7766a7ae-11ff-42f4-84e4-5c27f939f4d8:#{Time.new.to_s[0, 10]}", "[]")
-        points = JSON.parse(points)
-        points << Time.new.to_i
-        KeyValueStore::set(nil, "7766a7ae-11ff-42f4-84e4-5c27f939f4d8:#{Time.new.to_s[0, 10]}", JSON.generate(points))
-    end
-
-    # YouTubeVideoStream::metric()
-    def self.metric()
-        points = KeyValueStore::getOrDefaultValue(nil, "7766a7ae-11ff-42f4-84e4-5c27f939f4d8:#{Time.new.to_s[0, 10]}", "[]")
-        points = JSON.parse(points)
-        return 0.7 if points.empty?
-        n = points.select{|point| (Time.new.to_i-point) < 3600*2 }.count
-        0.7*Math.exp(-n.to_f/6)
-    end
-
     # YouTubeVideoStream::videoFolderpathsAtFolder(folderpath)
     def self.videoFolderpathsAtFolder(folderpath)
         return [] if !File.exists?(folderpath)
@@ -60,31 +43,6 @@ class YouTubeVideoStream
             .select{|filename| filename[0,1] != "." }
             .map{|filename| "#{folderpath}/#{filename}" }
             .sort
-    end
-
-    # YouTubeVideoStream::calalystObjects()
-    def self.calalystObjects()
-
-        filepath = YouTubeVideoStream::videoFolderpathsAtFolder(YouTubeVideoStream::spaceFolderpath()).first
-        return [] if filepath.nil?
-        uuid = "f7845869-e058-44cd-bfae-3412957c7dba"
-        return [] if !DoNotShowUntil::isVisible(uuid)
-        announce = "YouTube Video Stream"
-        [
-            {
-                "uuid"                => uuid,
-                "contentItem"         => {
-                    "type" => "line",
-                    "line" => "YouTube Video Stream"
-                },
-                "metric"              => YouTubeVideoStream::metric(),
-                "commands"            => [],
-                "defaultCommand"      => "view",
-                "shell-redirects" => {
-                    "view"  => "/Users/pascal/Galaxy/LucilleOS/Applications/Catalyst/YouTubeVideoStream/catalyst-objects-processing view"
-                }
-            }
-        ]
     end
 
     # YouTubeVideoStream::filepathToVideoUUID(filepath)
