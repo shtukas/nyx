0bc7a6ef9bffd48391696189449ce848e85ca7d5
Pascal Honoré <pascal@alseyn.net>
2020-07-05 10:53:11 +0100

    Reimplement NyxObjectsCacheOperator

--- NyxObjects.rb
+++ NyxObjects.rb
@@ -18,6 +18,26 @@ require 'securerandom'
 # SecureRandom.hex(4) #=> "eb693123"
 # SecureRandom.uuid   #=> "2d931510-d99f-494a-8c67-87feb05e1594"
 
+require "/Users/pascal/Galaxy/LucilleOS/Libraries/Ruby-Libraries/KeyValueStore.rb"
+=begin
+    KeyValueStore::setFlagTrue(repositorylocation or nil, key)
+    KeyValueStore::setFlagFalse(repositorylocation or nil, key)
+    KeyValueStore::flagIsTrue(repositorylocation or nil, key)
+
+    KeyValueStore::set(repositorylocation or nil, key, value)
+    KeyValueStore::getOrNull(repositorylocation or nil, key)
+    KeyValueStore::getOrDefaultValue(repositorylocation or nil, key, defaultValue)
+    KeyValueStore::destroy(repositorylocation or nil, key)
+=end
+
+require "/Users/pascal/Galaxy/LucilleOS/Libraries/Ruby-Libraries/BTreeSets.rb"
+=begin
+    BTreeSets::values(repositorylocation or nil, setuuid: String): Array[Value]
+    BTreeSets::set(repositorylocation or nil, setuuid: String, valueuuid: String, value)
+    BTreeSets::getOrNull(repositorylocation or nil, setuuid: String, valueuuid: String): nil | Value
+    BTreeSets::destroy(repositorylocation, setuuid: String, valueuuid: String)
+=end
+
 # ------------------------------------------------------------------------
 
 class NyxObjects
@@ -50,8 +70,8 @@ class NyxObjects
         raise "[NyxPrimaryStoreUtils: a10f1670-b694-4937-b155-cbfa695b784a]"
     end
 
-    # NyxObjects::storedObjectsEnumerator()
-    def self.storedObjectsEnumerator()
+    # NyxObjects::primaryStoreObjectsEnumerator()
+    def self.primaryStoreObjectsEnumerator()
         Enumerator.new do |objects|
             Find.find("#{CatalystCommon::catalystDataCenterFolderpath()}/Nyx-Objects") do |path|
                 next if !File.file?(path)
@@ -61,10 +81,10 @@ class NyxObjects
         end
     end
 
-    # NyxObjects::loadObjectsLatestVersions()
-    def self.loadObjectsLatestVersions()
+    # NyxObjects::objects()
+    def self.objects()
         mapping = {}
-        NyxObjects::storedObjectsEnumerator()
+        NyxObjects::primaryStoreObjectsEnumerator()
             .each{|object|
                 if mapping[object["uuid"]].nil? then
                     mapping[object["uuid"]] = object
@@ -77,6 +97,7 @@ class NyxObjects
         mapping
             .values
             .select{|object| !object["nyxNxSet"].nil? } # removing the ones that have been deleted
+            .select{|object| NyxObjects::nyxNxSets().include?(object["nyxNxSet"]) } # only select the one from alive sets
     end
 
     # Public Interface
@@ -97,18 +118,17 @@ class NyxObjects
         namedhash = "SHA256-#{Digest::SHA256.hexdigest(blob)}"
         filepath = NyxObjects::namedHashToObjectsFilepath(namedhash)
         File.open(filepath, "w") {|f| f.write(blob) }
-        $X9176ffbef04a.put(object)
-        namedhash
+        NyxObjectsCacheOperator::put(object)
     end
 
     # NyxObjects::getOrNull(uuid)
     def self.getOrNull(uuid)
-        $X9176ffbef04a.getOrNull(uuid)
+        NyxObjectsCacheOperator::getOrNull(uuid)
     end
 
     # NyxObjects::getSet(setid)
     def self.getSet(setid)
-        $X9176ffbef04a.objects(setid)
+        NyxObjectsCacheOperator::objects(setid)
     end
 
     # NyxObjects::destroy(uuid)
@@ -121,50 +141,87 @@ class NyxObjects
         namedhash = "SHA256-#{Digest::SHA256.hexdigest(blob)}"
         filepath = NyxObjects::namedHashToObjectsFilepath(namedhash)
         File.open(filepath, "w") {|f| f.write(blob) }
-        $X9176ffbef04a.put(object)
-        namedhash
-        $X9176ffbef04a.destroyObject(uuid)
-    end
-
-end
-
-class NyxObjectsInMemoryOperator
-
-    # @allObjectsInMemory = {}
-
-    def initialize()
-        @allObjectsInMemory = {}
-        NyxObjects::loadObjectsLatestVersions()
-            .each{|object|
-                @allObjectsInMemory[object["uuid"]] = object
-            }
-    end
-
-    def put(object)
-        @allObjectsInMemory[object["uuid"]] = object.clone
-    end
-
-    def getOrNull(uuid)
-        @allObjectsInMemory
-            .values
-            .select{|object| object["uuid"] == uuid }
-            .map{|object| object.clone }
-            .first
-    end
-
-    def objects(setid)
-        @allObjectsInMemory
-            .values
-            .select{|object| object["nyxNxSet"] == setid }
-            .map{|object| object.clone }
-    end
-
-    def destroyObject(uuid)
-        @allObjectsInMemory.delete(uuid)
+        NyxObjectsCacheOperator::destroy(uuid)
     end
 end
 
-if !defined?($X9176ffbef04a) then
-    $X9176ffbef04a = NyxObjectsInMemoryOperator.new()
+if !defined?($InMemoryObjectsCache6DB420D8) then
+    $InMemoryObjectsCache6DB420D8 = {} # Map[uuid: String, Object]
 end
 
+if !defined?($InMemorySetsCache2917988) then
+    $InMemorySetsCache2917988 = {} # Map[set: String, Map[uuid: String, Object] ]
+    NyxObjects::nyxNxSets().each{|setid|
+        $InMemorySetsCache2917988[setid] = {}
+    }
+end 
+
+class NyxObjectsCacheOperator
+
+    # NyxObjectsCacheOperator::put(object)
+    def self.put(object)
+        # This is called everytime an object mutates, this is the entry point of the caching system
+
+        # We update the in memory cache
+        $InMemoryObjectsCache6DB420D8[object["uuid"]] = object
+
+        # We update the in memory set cache
+        # If the in memory set has been initialised
+        if $InMemorySetsCache2917988[object["nyxNxSet"]] then
+            $InMemorySetsCache2917988[object["nyxNxSet"]][object["uuid"]] = object
+        end
+
+        # We update the on disk cache
+        KeyValueStore::set(nil, "9a470ad8-ab23-4a51-b94d-195de9912da7:#{object["uuid"]}", JSON.generate(object))
+
+        # We update the set objects
+        BTreeSets::set(nil, "d9d54faa-d3e4-41e1-a9e0-317ba20e3884:#{object["nyxNxSet"]}", object["uuid"], object)
+    end
+
+    # NyxObjectsCacheOperator::getOrNull(uuid)
+    def self.getOrNull(uuid)
+        if $InMemoryObjectsCache6DB420D8[uuid] then
+            return $InMemoryObjectsCache6DB420D8[uuid].clone
+        end
+
+        object = KeyValueStore::getOrNull(nil, "9a470ad8-ab23-4a51-b94d-195de9912da7:#{uuid}")
+        if object then
+            object = JSON.parse(object)
+            $InMemoryObjectsCache6DB420D8[object["uuid"]] = object
+            return object
+        end
+
+        nil
+    end
+
+    # NyxObjectsCacheOperator::objects(setid)
+    def self.objects(setid)
+        if $InMemorySetsCache2917988[setid].nil? then
+            $InMemorySetsCache2917988[setid] = {}
+            BTreeSets::values(nil, "d9d54faa-d3e4-41e1-a9e0-317ba20e3884:#{setid}")
+                .each{|object|
+                    $InMemorySetsCache2917988[setid][object["uuid"]] = object
+                }
+        end
+        $InMemorySetsCache2917988[setid].values.map{|object| object.clone }
+    end
+
+    # NyxObjectsCacheOperator::destroy(uuid)
+    def self.destroy(uuid)
+        $InMemoryObjectsCache6DB420D8.delete(uuid)
+        KeyValueStore::destroy(nil, "9a470ad8-ab23-4a51-b94d-195de9912da7:#{uuid}")
+        NyxObjects::nyxNxSets().each{|setid|
+            BTreeSets::destroy(nil, "d9d54faa-d3e4-41e1-a9e0-317ba20e3884:#{setid}", uuid)
+        }
+    end
+
+    # NyxObjectsCacheOperator::reCacheAll()
+    def self.reCacheAll()
+        NyxObjects::objects().each{|object|
+            puts JSON.pretty_generate(object)
+            KeyValueStore::set(nil, "9a470ad8-ab23-4a51-b94d-195de9912da7:#{object["uuid"]}", JSON.generate(object))
+            BTreeSets::set(nil, "d9d54faa-d3e4-41e1-a9e0-317ba20e3884:#{object["nyxNxSet"]}", object["uuid"], object)
+        }
+    end
+
+end
