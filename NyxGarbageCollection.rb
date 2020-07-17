ba41d6fbd8170faed333fcb7dc228e6bd892de71
Pascal Honoré <pascal@alseyn.net>
2020-07-17 16:49:35 +0100

    Update the garbage collector

--- NyxGarbageCollection.rb
+++ NyxGarbageCollection.rb
@@ -4,15 +4,48 @@ class NyxGarbageCollection
 
     # NyxGarbageCollection::run()
     def self.run()
+        
         puts "NyxGarbageCollection::run()"
+        
         Arrows::arrows().each{|arrow|
             b1 = NyxPrimaryObjects::getOrNull(arrow["sourceuuid"]).nil?
             b2 = NyxPrimaryObjects::getOrNull(arrow["targetuuid"]).nil?
             isNotConnecting = (b1 or b2)
             if isNotConnecting then
-                puts "removing: #{arrow}"
+                puts "removing arrow: #{arrow}"
                 NyxObjects::destroy(arrow["uuid"])
             end
         }
+
+        DescriptionZ::descriptionz().each{|descriptionz|
+            next if Arrows::getSourcesForTarget(descriptionz).size > 0
+            puts "removing descriptionz: #{descriptionz}"
+            NyxObjects::destroy(descriptionz["uuid"])
+        }
+
+        DateTimeZ::datetimez().each{|datetimez|
+            next if Arrows::getSourcesForTarget(datetimez).size > 0
+            puts "removing datetimez: #{datetimez}"
+            NyxObjects::destroy(datetimez["uuid"])
+        }
+
+        Tags::tags().each{|tag|
+            next if Arrows::getSourcesForTarget(tag).size > 0
+            puts "removing tag: #{tag}"
+            NyxObjects::destroy(tag["uuid"])
+        }
+
+        Notes::notes().each{|note|
+            next if Arrows::getSourcesForTarget(note).size > 0
+            puts "removing note: #{note}"
+            NyxObjects::destroy(note["uuid"])
+        }
+
+        Comments::comments().each{|comment|
+            next if Arrows::getSourcesForTarget(comment).size > 0
+            puts "removing comment: #{comment}"
+            NyxObjects::destroy(comment["uuid"])
+        }
+
     end
 end
