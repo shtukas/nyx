dfde4ab034f9d60187c22d62776dbf9e60f77b92
Pascal Honoré <pascal@alseyn.net>
2020-07-25 19:02:19 +0100

    Correct primaryDataStoreFolderPath for docnet

--- src/Ruby/DataStore2DataStore3DirectionalSyncs.rb
+++ src/Ruby/DataStore2DataStore3DirectionalSyncs.rb
@@ -45,7 +45,7 @@ class DataStore2DataStore3DirectionalSyncs
         raise "[error: a5e85d2a]" if !Realms::primaryDataStoreFolderPath().include?("002-primary-store-949658fc-5474-45cf-b754-ab2500a89a93")
         # I confirm, we really do not want this to run against the catalyst primary data store
         # ------------------------------------------------------------------------------------
-        Find.find("#{Realms::primaryDataStoreFolderPath()}/Nyx/Nyx-Objects") do |path|
+        Find.find("#{Realms::primaryDataStoreFolderPath()}/Nyx-Objects") do |path|
             next if !File.file?(path)
             next if path[-5, 5] != ".json"
             filename = File.basename(path)
@@ -54,7 +54,7 @@ class DataStore2DataStore3DirectionalSyncs
             puts "copying: #{File.basename(path)}"
             FileUtils.cp(path, targetFilepath)
         end
-        Find.find("#{Realms::primaryDataStoreFolderPath()}/Nyx/Nyx-Blobs") do |path|
+        Find.find("#{Realms::primaryDataStoreFolderPath()}/Nyx-Blobs") do |path|
             next if !File.file?(path)
             next if path[-5, 5] != ".data"
             filename = File.basename(path)
@@ -72,7 +72,7 @@ class DataStore2DataStore3DirectionalSyncs
         # 000c1c5d63c2325b129768131b19be20dfe593cb4e97511545dd4e9912950b65.json
         ns01 = filename[0, 2]
         ns02 = filename[2, 2]
-        filepath = "#{Realms::primaryDataStoreFolderPath()}/Nyx/Nyx-Objects/#{ns01}/#{ns02}/#{filename}"
+        filepath = "#{Realms::primaryDataStoreFolderPath()}/Nyx-Objects/#{ns01}/#{ns02}/#{filename}"
         if !File.exists?(File.dirname(filepath)) then
             FileUtils.mkpath(File.dirname(filepath))
         end
@@ -86,7 +86,7 @@ class DataStore2DataStore3DirectionalSyncs
         # SHA256-00008077382c6e8e2763c416bb703bf39ef2227b85b4671d107f5d92b629c2d7.data
         ns01 = filename[7, 2]
         ns02 = filename[9, 2]
-        filepath = "#{Realms::primaryDataStoreFolderPath()}/Nyx/Nyx-Blobs/#{ns01}/#{ns02}/#{filename}"
+        filepath = "#{Realms::primaryDataStoreFolderPath()}/Nyx-Blobs/#{ns01}/#{ns02}/#{filename}"
         if !File.exists?(File.dirname(filepath)) then
             FileUtils.mkpath(File.dirname(filepath))
         end
