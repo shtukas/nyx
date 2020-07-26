dfed559dfbcf992b27f0ab7d47ad8851922945d5
Pascal Honoré <pascal@alseyn.net>
2020-07-26 22:03:54 +0100

    update/repairs in Interactive Search

--- src/Ruby/GraphTypes.rb
+++ src/Ruby/GraphTypes.rb
@@ -369,73 +369,88 @@ class GraphTypes
         win2.refresh
         win3.refresh
 
-        display_string_558ca20d = ""
-        search_queue           = []
-        selected_objects         = []
+        win1_display_string = ""
+        search_string       = nil # string or nil
+        search_string_last_time_update = nil
 
-        thread1 = Thread.new {
+        selected_objects    = []
+
+        display_search_string = lambda {
+            win1.setpos(0,0)
+            win1.deleteln()
+            win1 << ("-> " + (win1_display_string || ""))
+            win1.refresh
+        }
+
+        display_searching_on = lambda {
+            win2.setpos(0,0)
+            win2.deleteln()
+            win2 << "searching..."
+            win2.refresh
+        }
+        display_searching_off = lambda {
+            win2.setpos(0,0)
+            win2.deleteln()
+            win2.refresh
+        }
+
+        thread4 = Thread.new {
             loop {
-                win1.setpos(0,0) # we set the cursor on the starting position
-                win1.deleteln()
-                win1 << "search: #{display_string_558ca20d}"
-                win1.refresh
+
                 sleep 0.01
-            }
-        }
 
-        thread2 = Thread.new {
-            loop {
-                win2.setpos(0,0)
-                win2.deleteln()
-                win2 << "search_queue: #{search_queue.join(" | ")}"
-                win2.refresh
-                sleep 0.1
-            }
-        }
+                next if search_string.nil?
+                next if search_string_last_time_update.nil?
+                next if (Time.new.to_f - search_string_last_time_update) < 1
 
-        thread3 = Thread.new {
-            loop {
-                sleep 1
+                pattern = search_string
+                search_string = nil
+
+                display_searching_on.call()
+                selected_objects = GraphTypes::selectObjectsUsingPattern(pattern)
+
                 win3.setpos(0,0)
-                selected_objects.first(40).each{|object|
+                selected_objects.first(Miscellaneous::screenHeight()-3).each{|object|
                     win3.deleteln()
                     win3 << "#{GraphTypes::toString(object)}\n"
                 }
                 (win3.maxy - win3.cury).times {win3.deleteln()}
                 win3.refresh
+
+                display_searching_off.call()
+                display_search_string.call()
             }
         }
 
-        thread4 = Thread.new {
-            loop {
-                sleep 0.1
-                next if search_queue.empty?
-                str = search_queue.shift
-                next if str == 0
-                selected_objects = GraphTypes::selectObjectsUsingPattern(str)
-            }
-        }
+        display_search_string.call()
 
         loop {
-            char = win1.getch.to_s # Reads and returobject a character
+
+            char = win1.getch.to_s # Reads and return a character non blocking
+
+            next if char.size == 0
+
             if char == '127' then
                 # delete
-                next if display_string_558ca20d.length == 0
-                display_string_558ca20d = display_string_558ca20d[0, display_string_558ca20d.length-1]
-                search_queue << display_string_558ca20d
+                next if win1_display_string.length == 0
+                win1_display_string = win1_display_string[0, win1_display_string.length-1]
+                search_string = win1_display_string
+                search_string_last_time_update = Time.new.to_f
+                display_search_string.call()
                 next
             end
+
             if char == '10' then
                 # enter
                 break
             end
-            display_string_558ca20d << char
-            search_queue << display_string_558ca20d
+
+            win1_display_string << char
+            search_string = win1_display_string
+            search_string_last_time_update = Time.new.to_f
+            display_search_string.call()
         }
 
-        Thread.kill(thread1)
-        Thread.kill(thread2)
-        Thread.kill(thread3)
         Thread.kill(thread4)
 
         win1.close
