66c9e174149a47a7805801897beb8eb0f0c64e70
Pascal Honoré <pascal@alseyn.net>
2020-08-08 21:54:18 +0100

    VideoStream updates

--- src/Ruby/VideoStream.rb
+++ src/Ruby/VideoStream.rb
@@ -34,6 +34,12 @@ class VideoStream
         0.2 + 0.7*Math.exp(-BankExtended::recoveredDailyTimeInHours("VideoStream-3623a0c2-ef0d-47e2-9008-3c1a9fd52c01")) - indx.to_f/1000000
     end
 
+    # VideoStream::videoIsRunning(filepath)
+    def self.videoIsRunning(filepath)
+        uuid = "8410f77a-0624-442d-b413-f2be0fcce5ba:#{filepath}"
+        Runner::isRunning?(uuid)
+    end
+
     # VideoStream::catalystObjects()
     def self.catalystObjects()
 
@@ -45,10 +51,11 @@ class VideoStream
             .first(3)
             .map
             .with_index{|filepath, indx|
+                isRunning = VideoStream::videoIsRunning(filepath)
                 uuid = VideoStream::filepathToVideoUUID(filepath)
                 objects << {
                     "uuid"        => uuid,
-                    "body"        => "[VideoStream] #{File.basename(filepath)}",
+                    "body"        => "[VideoStream] #{File.basename(filepath)}#{isRunning ? " (running)" : ""}",
                     "metric"      => VideoStream::metric(indx),
                     "execute"     => lambda { |input| VideoStream::execute(filepath) },
                     "x-video-stream" => true,
@@ -56,41 +63,46 @@ class VideoStream
                 }
             }
 
+        if objects.any?{|object| object["body"].include?("running") } then
+            objects = objects.select{|object| object["body"].include?("running") }
+        end
+
         objects
     end
 
-    # VideoStream::play(filepath)
-    def self.play(filepath)
-        startTime = Time.new.to_f
+    # VideoStream::playStopComplete(filepath)
+    def self.playStopComplete(filepath)
         puts filepath
-        if filepath.include?("'") then
-            filepath2 = filepath.gsub("'", ',')
-            FileUtils.mv(filepath, filepath2)
-            filepath = filepath2
+        uuid = "8410f77a-0624-442d-b413-f2be0fcce5ba:#{filepath}"
+        isRunning = Runner::isRunning?(uuid)
+        if isRunning then
+            if LucilleCore::askQuestionAnswerAsBoolean("-> completed? ", true) then
+                FileUtils.rm(filepath)
+            end
+            timespan = Runner::stop(uuid)
+            puts "Watched for #{timespan} seconds"
+            Bank::put("VideoStream-3623a0c2-ef0d-47e2-9008-3c1a9fd52c01", timespan)
+        else
+            options = ["play", "completed"]
+            option = LucilleCore::selectEntityFromListOfEntitiesOrNull("operation", options)
+            if option == "play" then
+                Runner::start(uuid)
+                system("open '#{filepath}'")
+            end
+            if option == "completed" then
+                FileUtils.rm(filepath)
+            end
         end
-        system("open '#{filepath}'")
-        if LucilleCore::askQuestionAnswerAsBoolean("-> completed? ", true) then
-            FileUtils.rm(filepath)
-            sleep 1
-        end
-        watchTime = Time.new.to_f - startTime
-        puts "Watched for #{watchTime} seconds"
-        Bank::put("VideoStream-3623a0c2-ef0d-47e2-9008-3c1a9fd52c01", watchTime)
     end
 
     # VideoStream::execute(filepath)
     def self.execute(filepath)
-        options = ["play", "completed"]
-        option = LucilleCore::selectEntityFromListOfEntitiesOrNull("operation", options)
-        if option == "play" then
-            VideoStream::play(filepath)
+        if filepath.include?("'") then
+            filepath2 = filepath.gsub("'", ' ')
+            FileUtils.mv(filepath, filepath2)
+            filepath = filepath2
         end
-        if option == "completed" then
-            exit if filepath.nil?
-            puts filepath
-            FileUtils.rm(filepath)
-        end
+        VideoStream::playStopComplete(filepath)
     end
-
 end
 
