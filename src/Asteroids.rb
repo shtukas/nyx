6154e51a210b300eba7a0d9ccdb8207c5331800f
Pascal Honoré <pascal@alseyn.net>
2020-09-09 16:26:11 +0100

    Destroy asteroid before landing

--- src/Asteroids.rb
+++ src/Asteroids.rb
@@ -430,9 +430,10 @@ class Asteroids
 
                 Arrows::issueOrException(node, target) 
             }
+        NyxObjects2::destroy(asteroid) # We destroy the asteroid itself and not doing Asteroids::destroy(asteroid) because we are keeping the children by default.
         SelectionLookupDataset::updateLookupForNode(node)
         NSDataPoint::landing(node)
-        NyxObjects2::destroy(asteroid) # We destroy the asteroid itself and not doing Asteroids::destroy(asteroid) because we are keeping the children by default.
+
     end
 
     # Asteroids::diveAsteroidOrbitalType(orbitalType)
