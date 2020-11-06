a50223235a54bcf9fb570607dc8ea2750f08dd38
Pascal Honoré <pascal@alseyn.net>
2020-11-06 17:33:28 +0000

    Fsck update

--- src/NyxFsck.rb
+++ src/NyxFsck.rb
@@ -5,7 +5,6 @@ class NyxFsck
 
     # NyxFsck::processNGX15(datapoint)
     def self.processNGX15(datapoint)
-        puts "fsck NGX15: #{datapoint["uuid"]}"
         code = datapoint["ngx15"]
         puts "Finding #{code}"
         location = GalaxyFinder::uniqueStringToLocationOrNull(code)
@@ -20,7 +19,6 @@ class NyxFsck
 
     # NyxFsck::processQuark(quark)
     def self.processQuark(quark)
-        puts "fsck quark: #{quark["uuid"]}"
         leptonfilename = quark["leptonfilename"]
         leptonfilepath = LeptonsFunctions::leptonFilenameToFilepath(leptonfilename)
         File.exists?(leptonfilepath)
@@ -31,21 +29,25 @@ class NyxFsck
 
         if object["nyxNxSet"] == "b66318f4-2662-4621-a991-a6b966fb4398" then
             # Asteroid
+            puts "fsck asteroid: #{object["uuid"]}"
             return true
         end
 
         if object["nyxNxSet"] == "7deb0315-98b5-4e4d-9ad2-d83c2f62e6d4" then
             # Wave
+            puts "fsck wave: #{object["uuid"]}"
             return true
         end
 
         if object["nyxNxSet"] == "0f555c97-3843-4dfe-80c8-714d837eba69" then
             # Datapoint
+            puts "fsck NGX15: #{datapoint["uuid"]}"
             return NyxFsck::processNGX15(object)
         end
 
         if object["nyxNxSet"] == "d65674c7-c8c4-4ed4-9de9-7c600b43eaab" then
             # Quark
+            puts "fsck quark: #{quark["uuid"]}"
             return NyxFsck::processQuark(object)
         end
 
@@ -64,7 +66,6 @@ class NyxFsck
     def self.main(runhash)
         NyxObjects2::getAllObjects().each{|object|
             next if KeyValueStore::flagIsTrue(nil, "#{runhash}:#{object["uuid"]}")
-            puts "fsck object: #{object["uuid"]}"
             status = NyxFsck::processObject(object, runhash)
             return false if !status
             KeyValueStore::setFlagTrue(nil, "#{runhash}:#{object["uuid"]}")
