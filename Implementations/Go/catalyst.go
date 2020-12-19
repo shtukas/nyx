f596c9f021b88c396ff806890b6bcd48451ba8fd
Pascal Honoré <pascal@alseyn.net>
2020-12-19 20:05:26 +0000

    Re implementation in Go 1/.

--- Implementations/Go/catalyst.go
+++ Implementations/Go/catalyst.go
@@ -1,5 +1,19 @@
 package main
-import "fmt"
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
+
 func main() {
-    fmt.Println("hello world")
+    fmt.Println("24 hours presence ratio: 23.41 %, asteroids count: 3942, -3, 3942")
+    fmt.Println("")
+    fmt.Println("[19] NG12TimeReport [1.00 hours,  24.96 % completed] All asteroid burners")
+    fmt.Println("[21] NG12TimeReport [1.00 hours,  39.65 % completed] All asteroid streams")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, _ := reader.ReadString('\n')
+	fmt.Println(text)
 }
