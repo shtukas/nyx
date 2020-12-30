849deda8c86b55ec9f433018cd4eda57efa8d326
Pascal Honoré <pascal@alseyn.net>
2020-12-30 19:06:24 +0000

    Runnable Floats

--- Implementations/Ruby/Floats.rb
+++ Implementations/Ruby/Floats.rb
@@ -53,22 +53,50 @@ class Floats
                 "body"             => Floats::toString(float).yellow,
                 "metric"           => 0.2 + 0.7*EvaporatingWeights::getRatio(uuid),
                 "landing"          => lambda {
+                    uuid = float["uuid"]
                     operations = [
+                        "start",
                         "destroy"
                     ]
                     operation = LucilleCore::selectEntityFromListOfEntitiesOrNull("operation", operations)
                     return if operation.nil?
+                    if operation == "start" then
+                        Runner::start(uuid)
+                    end
                     if operation == "destroy" then
                         NSCoreObjects::destroy(float)
                     end
                 },
                 "nextNaturalStep"  => lambda {
-                    if LucilleCore::askQuestionAnswerAsBoolean("destroy '#{Floats::toString(float)}' ? ") then
-                        NSCoreObjects::destroy(float)
+                    uuid = float["uuid"]
+                    if Runner::isRunning?(uuid) then
+                        operations = [
+                            "stop",
+                            "stop and destroy"
+                        ]
+                        operation = LucilleCore::selectEntityFromListOfEntitiesOrNull("operation", operations)
+                        return if operation.nil?
+                        if operation == "stop" then
+                            dxthread = DxThreads::selectOneExistingDxThreadOrNull()
+                            timespan = Runner::stop(uuid)
+                            timespan = [timespan, 3600*2].min # To avoid problems after leaving things running
+                            puts "sending #{timespan} to '#{DxThreads::toString(dxthread)}'"
+                            Bank::put(dxthread["uuid"], timespan)
+                        end
+                        if operation == "stop and destroy" then
+                            dxthread = DxThreads::selectOneExistingDxThreadOrNull()
+                            timespan = Runner::stop(uuid)
+                            timespan = [timespan, 3600*2].min # To avoid problems after leaving things running
+                            puts "sending #{timespan} to '#{DxThreads::toString(dxthread)}'"
+                            Bank::put(dxthread["uuid"], timespan)
+                            NSCoreObjects::destroy(float)
+                        end
+                    else
+                        Runner::start(uuid)
                     end
                 },
-                "isRunning"          => false,
-                "isRunningForLong"   => false,
+                "isRunning"          => Runner::isRunning?(uuid),
+                "isRunningForLong"   => (Runner::runTimeInSecondsOrNull(uuid) || 0) > 3600,
                 "x-isFloat"          => true,
                 "x-float-add-weight" => lambda { EvaporatingWeights::mark(uuid) }
             }
