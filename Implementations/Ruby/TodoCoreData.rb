ae5e4115aaff1114b5bf7053b9af224a97c20dff
Pascal Honoré <pascal@alseyn.net>
2021-02-04 09:44:34 +0000

    TodoCoreData refactoring

--- Implementations/Ruby/TodoCoreData.rb
+++ Implementations/Ruby/TodoCoreData.rb
@@ -1,7 +1,5 @@
 
 
-$TodoCoreDataCache76DBF964 = {}
-
 class TodoCoreData
 
     # TodoCoreData::databaseFilepath()
@@ -12,7 +10,7 @@ class TodoCoreData
     # TodoCoreData::getAllObjects()
     def self.getAllObjects()
         db = SQLite3::Database.new(TodoCoreData::databaseFilepath())
-        db.busy_timeout = 117  
+        db.busy_timeout = 117
         db.busy_handler { |count| true }
         db.results_as_hash = true
         answer = []
@@ -26,25 +24,33 @@ class TodoCoreData
     # TodoCoreData::put(object)
     def self.put(object)
         db = SQLite3::Database.new(TodoCoreData::databaseFilepath())
-        db.busy_timeout = 117  
+        db.busy_timeout = 117
         db.busy_handler { |count| true }
         db.transaction 
         db.execute "delete from table2 where _objectuuid_=?", [object["uuid"]]
         db.execute "insert into table2 (_setuuid_, _objectuuid_, _object_) values ( ?, ?, ? )", [object["nyxNxSet"], object["uuid"], JSON.generate(object)]
         db.commit 
         db.close
-        $TodoCoreDataCache76DBF964[object["uuid"]] = object
     end
 
     # TodoCoreData::getOrNull(uuid)
     def self.getOrNull(uuid)
-        $TodoCoreDataCache76DBF964[uuid]
+        db = SQLite3::Database.new(TodoCoreData::databaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        answer = nil
+        db.execute( "select * from table2 where _objectuuid_=?" , [uuid] ) do |row|
+            answer = JSON.parse(row['_object_'])
+        end
+        db.close
+        answer
     end
 
     # TodoCoreData::getSet(setid)
     def self.getSet(setid)
         db = SQLite3::Database.new(TodoCoreData::databaseFilepath())
-        db.busy_timeout = 117  
+        db.busy_timeout = 117
         db.busy_handler { |count| true }
         db.results_as_hash = true
         answer = []
@@ -58,15 +64,10 @@ class TodoCoreData
     # TodoCoreData::destroy(object)
     def self.destroy(object)
         db = SQLite3::Database.new(TodoCoreData::databaseFilepath())
-        db.busy_timeout = 117  
+        db.busy_timeout = 117
         db.busy_handler { |count| true }
         db.execute "delete from table2 where _objectuuid_=?", [object["uuid"]]
         db.close
-        $TodoCoreDataCache76DBF964.delete(object["uuid"])
         Ordinals::deleteRecord(object["uuid"])
     end
 end
-
-TodoCoreData::getAllObjects().each{|object|
-    $TodoCoreDataCache76DBF964[object["uuid"]] = object
-}
