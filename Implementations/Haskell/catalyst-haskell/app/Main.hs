a6ccd57725727c1c5b077c091e2b555f35439bd4
Pascal Honoré <pascal@alseyn.net>
2021-04-04 14:09:37 +0100

    Code reorganization

--- Implementations/Haskell/catalyst-haskell/app/Main.hs
+++
@@ -1,8 +0,0 @@
-module Main where
-
-import UserInterface.Types as UI
-
-main :: IO ()
-main = do
-        putStrLn "[]"
-        putStrLn $ show $ UI.NS16 "d0efe9b6-886d-4295-b4f2-f756fd9cf355" "Testing from Haskell"
