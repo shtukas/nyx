b003ac1051078915c7cf42b6424b198b674dbce7
Pascal Honoré <pascal@alseyn.net>
2021-04-22 11:00:26 +0100

    ns16 reordering

--- Libs/UIServices.rb
+++ Libs/UIServices.rb
@@ -103,7 +103,7 @@ class UIServices
     def self.catalystNS16s()
         isWorkTime = ([1,2,3,4,5].include?(Time.new.wday) and (9..16).to_a.include?(Time.new.hour) and !KeyValueStore::flagIsTrue(nil, "a2f220ce-e020-46d9-ba64-3938ca3b69d4:#{Utils::today()}"))
         if isWorkTime then
-            return WorkInterface::ns16s() + UIServices::waveLikeNS16s() + [ UIServices::todayNS16OrNull() ].compact + UIServices::quarksAndSyntheticNS16s()
+            return UIServices::waveLikeNS16s() + WorkInterface::ns16s() + [ UIServices::todayNS16OrNull() ].compact + UIServices::quarksAndSyntheticNS16s()
         end
         UIServices::waveLikeNS16s() + [ UIServices::todayNS16OrNull() ].compact  + UIServices::quarksAndSyntheticNS16s()
     end
