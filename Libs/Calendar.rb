136f8038aef63c0ae6cf829ad1c16dab61a587f5
Pascal Honoré <pascal@alseyn.net>
2021-05-20 13:44:27 +0100

    Calendar update

--- Libs/Calendar.rb
+++ Libs/Calendar.rb
@@ -7,10 +7,17 @@ class Calendar
         "/Users/pascal/Galaxy/Calendar"
     end
 
+    # Calendar::pathToArchivesFolder()
+    def self.pathToArchivesFolder()
+        folderpath = "/Users/pascal/Galaxy/Documents/06 Timeline/#{Time.new.strftime("%Y")}/Calendar (Catalyst)"
+        raise "d1d363cb-1b34-4091-bd87-455c96ab16e2" if File.exists?(folderpath)
+        folderpath
+    end
+
     # Calendar::issue(date, description)
     def self.issue(date, description)
         foldername = "#{date} | #{description}"
-        folderpath = "#{Calendar::pathToCalendarFolder()}/02-Future/#{foldername}"
+        folderpath = "#{Calendar::pathToCalendarFolder()}/#{foldername}"
         if File.exists?(folderpath) then
             puts "I can't create item #{foldername}, somehow already exists."
             LucilleCore::pressEnterToContinue()
@@ -30,7 +37,7 @@ class Calendar
 
     # Calendar::items()
     def self.items()
-        LucilleCore::locationsAtFolder("#{Calendar::pathToCalendarFolder()}/02-Future").map{|folderpath|
+        LucilleCore::locationsAtFolder(Calendar::pathToCalendarFolder()).map{|folderpath|
             pair = File.basename(folderpath).split("|").map{|s| s.strip }
             {
                 "date" => pair[0],
@@ -47,7 +54,7 @@ class Calendar
 
     # Calendar::moveToArchives(item)
     def self.moveToArchives(item)
-        FileUtils.mv(item["folderpath"], "#{Calendar::pathToCalendarFolder()}/01-Archives")
+        FileUtils.mv(item["folderpath"], Calendar::pathToArchivesFolder())
     end
 
     # -----------------------------------------------------
