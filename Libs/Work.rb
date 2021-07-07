addc66384aca66b62a9509fd83505bfaf2a50c8f
Pascal Honoré <pascal@alseyn.net>
2021-07-07 01:52:59 +0100

    Work circuit breaker

--- Libs/Work.rb
+++ Libs/Work.rb
@@ -5,8 +5,6 @@
 
 class Work
 
-    # --------------------------------------------------
-
     # Work::isWorkTime()
     def self.isWorkTime()
         b1 = (9 <= Time.new.hour  and Time.new.hour < 17)
@@ -14,10 +12,16 @@ class Work
         b1 and b2
     end
 
+    # Work::citcuitBreaker()
+    def self.citcuitBreaker()
+        Bank::valueOverTimespan("WORK-E4A9-4BCD-9824-1EEC4D648408", 3600*4) > 3600*3
+    end
+
     # Work::shouldDisplayWork()
     def self.shouldDisplayWork()
         return false if (KeyValueStore::getOrNull(nil, "ce621184-51d7-456a-8ad1-20e7d9acb350:#{Utils::today()}") == "ns:false")
         return false if !DoNotShowUntil::isVisible("WORK-E4A9-4BCD-9824-1EEC4D648408")
+        return false if Work::citcuitBreaker()
         Work::isWorkTime()
     end
 
