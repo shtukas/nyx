4d93f3553ee90a868ecd1127eb2980d35d5579b0
Pascal Honoré <pascal@alseyn.net>
2021-09-05 21:54:05 +0100

    Calendar update

--- Libs/Calendar.rb
+++ Libs/Calendar.rb
@@ -7,18 +7,20 @@ class Calendar
         "/Users/pascal/Galaxy/Calendar"
     end
 
+    # Calendar::pathToCurrentFolder()
+    def self.pathToCurrentFolder()
+        "#{Calendar::pathToCalendarFolder()}/02-Current"
+    end
+
     # Calendar::pathToArchivesFolder()
     def self.pathToArchivesFolder()
-        l1 = "/Users/pascal/Galaxy/Timeline"
-        folderpath = "#{l1}/#{Time.new.strftime("%Y")}/Calendar (Catalyst)"
-        raise "d1d363cb-1b34-4091-bd87-455c96ab16e2" if !File.exists?(folderpath) # This will happen first thing after midnight every year
-        folderpath
+        "#{Calendar::pathToCalendarFolder()}/01-Archives"
     end
 
     # Calendar::issue(date, description)
     def self.issue(date, description)
-        foldername = "#{date} | #{description}"
-        folderpath = "#{Calendar::pathToCalendarFolder()}/#{foldername}"
+        foldername = "#{date} #{description}"
+        folderpath = "#{Calendar::pathToCurrentFolder()}/#{foldername}"
         if File.exists?(folderpath) then
             puts "I can't create item #{foldername}, somehow already exists."
             LucilleCore::pressEnterToContinue()
@@ -29,7 +31,7 @@ class Calendar
 
     # Calendar::interactivelyIssueNewCalendarItem()
     def self.interactivelyIssueNewCalendarItem()
-        date = LucilleCore::askQuestionAnswerAsString("date(time, with dot) (empty to abort) : ")
+        date = LucilleCore::askQuestionAnswerAsString("date 2021-01-01 (time 1200) (empty to abort) : ")
         return if date == ""
         description = LucilleCore::askQuestionAnswerAsString("description (empty to abort) : ")
         return if description == ""
@@ -38,22 +40,21 @@ class Calendar
 
     # Calendar::items()
     def self.items()
-        LucilleCore::locationsAtFolder(Calendar::pathToCalendarFolder()).map{|folderpath|
-            pair = File.basename(folderpath).split("|").map{|s| s.strip }
+        LucilleCore::locationsAtFolder(Calendar::pathToCurrentFolder()).map{|folderpath|
+            basename = File.basename(folderpath)
+            date = basename[0, 10]
+            description =  basename
             {
-                "date" => pair[0],
-                "description" => "(#{pair[1]}) #{pair[2]}",
-                "folderpath" => folderpath
+                "date"        => date,
+                "description" => basename,
+                "folderpath"  => folderpath
             }
         }
     end
 
     # Calendar::toString(item)
     def self.toString(item)
-        folderpath = item["folderpath"]
-        hasElementsInFolder = LucilleCore::locationsAtFolder(folderpath).size > 0
-        folderStr = hasElementsInFolder ? " [Folder Elements]" : ""
-        "[calendar] (#{item["date"]}) #{item["description"]}#{folderStr}"
+        "[calendar] #{item["description"]}"
     end
 
     # Calendar::moveToArchives(item)
@@ -70,12 +71,10 @@ class Calendar
 
     # Calendar::run(item)
     def self.run(item)
-        folderpath = item["folderpath"]
         puts Calendar::toString(item)
-        if hasElementsInFolder and LucilleCore::askQuestionAnswerAsBoolean("access folder ? ") then
-            system("open '#{folderpath}'")
-            LucilleCore::pressEnterToContinue()
-        end
+        folderpath = item["folderpath"]
+        system("open '#{folderpath}'")
+        LucilleCore::pressEnterToContinue()
         if LucilleCore::askQuestionAnswerAsBoolean("done ? ") then
             Calendar::moveToArchives(item)
         end
@@ -87,7 +86,6 @@ class Calendar
             .select{|item| Calendar::itemIsForNS16s(item) }
             .map{|item|
                 folderpath = item["folderpath"]
-                hasElementsInFolder = LucilleCore::locationsAtFolder(folderpath).size > 0
                 uuid = Digest::SHA1.hexdigest("4dc9a277-8880-472e-a459-cf1d9b7b6604:#{item["date"]}:#{item["description"]}")
                 {
                     "uuid"     => uuid,
