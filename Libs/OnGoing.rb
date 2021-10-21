92173f9b6f859593a2c4832b1aa900613ccb8117
Pascal Honoré <pascal@alseyn.net>
2021-10-21 21:52:56 +0100

    Upgrade OnGoing with CoreData

--- Libs/OnGoing.rb
+++ Libs/OnGoing.rb
@@ -2,47 +2,59 @@
 
 class OnGoing
 
-    # OnGoing::ns16s()
-    def self.ns16s()
-
-        #{
-        #    "uuid"
-        #    "unixtime"
-        #    "description"
-        #}
-
-        BTreeSets::values(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9")
-            .sort{|i1, i2| i1["unixtime"] <=> i2["unixtime"] }
-            .map{|item|
-                {
-                    "uuid"     => item["uuid"],
-                    "unixtime" => item["unixtime"],
-                    "announce" => "[ on ] #{item["description"]}",
-                    "commands" => ["done"],
-                    "interpreter" => lambda{|command|
-                        if command == "done" then
-                            BTreeSets::destroy(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", item["uuid"])
-                        end
-                    },
-                    "run"      => lambda {
-                        if LucilleCore::askQuestionAnswerAsBoolean("destroy ? ", true) then
-                            BTreeSets::destroy(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", item["uuid"])
-                        end  
-                    }
-                }
-            }
-    end
-
     # OnGoing::makeNewFromDescription(description)
     def self.makeNewFromDescription(description)
         uuid = SecureRandom.uuid
         item = {
             "uuid"        => uuid,
             "unixtime"    => Time.new.to_i,
-            "description" => description
+            "description" => description,
+            "coreDataId"  => CoreData::interactivelyCreateANewDataObjectReturnIdOrNull()
         }
         BTreeSets::set(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", uuid, item)
         BTreeSets::getOrNull(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", uuid)
+    end
+
+    # OnGoing::items()
+    def self.items()
+        BTreeSets::values(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9")
+            .sort{|i1, i2| i1["unixtime"] <=> i2["unixtime"] }
+        #{
+        #    "uuid"
+        #    "unixtime"
+        #    "description"
+        #    "coreDataId"
+        #}
+    end
+
+    # OnGoing::toString(item)
+    def self.toString(item)
+        "[ on ] #{item["description"]} (#{CoreData::contentTypeOrNull(item["coreDataId"])})"
+    end
+
+    # OnGoing::ns16s()
+    def self.ns16s()
+        OnGoing::items().map{|item|
+            {
+                "uuid"     => item["uuid"],
+                "unixtime" => item["unixtime"],
+                "announce" => OnGoing::toString(item),
+                "commands" => ["done"],
+                "interpreter" => lambda{|command|
+                    if command == "done" then
+                        BTreeSets::destroy(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", item["uuid"])
+                    end
+                },
+                "run"      => lambda {
+                    system("clear")
+                    puts OnGoing::toString(item)
+                    CoreData::accessWithOptionToEdit(item["coreDataId"])
+                    if LucilleCore::askQuestionAnswerAsBoolean("destroy ? ", true) then
+                        BTreeSets::destroy(nil, "5f8226ce-87e0-45aa-8df7-15d36ec568d9", item["uuid"])
+                    end  
+                }
+            }
+        }
     end
 
     # OnGoing::nx19s()
