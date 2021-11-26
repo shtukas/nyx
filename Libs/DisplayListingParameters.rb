a1dfd16ab9ea30b7af5dcdfc567d0ca3f39d3e42
Pascal Honoré <pascal@alseyn.net>
2021-11-26 19:13:26 +0000

    Introduce Nathalie (4)

--- Libs/DisplayListingParameters.rb
+++ Libs/DisplayListingParameters.rb
@@ -4,38 +4,29 @@ class DisplayListingParameters
 
     # DisplayListingParameters::ns16sWithoutNx50s(domain)
     def self.ns16sWithoutNx50s(domain)
-
-        ns16 = []
-
-        if domain == "(eva)" then
-            ns16 = [
-                Anniversaries::ns16s(),
-                Calendar::ns16s(),
-                JSON.parse(`/Users/pascal/Galaxy/LucilleOS/Binaries/amanda-bin-monitor`),
-                JSON.parse(`/Users/pascal/Galaxy/LucilleOS/Binaries/fitness ns16s`),
-                DrivesBackups::ns16s(),
-                Waves::ns16s(domain),
-                Inbox::ns16s()
-            ]
-        end
-
-        if domain == "(work)" then
-            ns16 = [
-                Waves::ns16s(domain),
-            ]
-        end
-
-        if domain == "(jedi)" then
-            ns16 = [
-                Waves::ns16s(domain),
-            ]
-        end
-
-        ns16
+        [
+            Anniversaries::ns16s(),
+            Calendar::ns16s(),
+            JSON.parse(`/Users/pascal/Galaxy/LucilleOS/Binaries/amanda-bin-monitor`),
+            JSON.parse(`/Users/pascal/Galaxy/LucilleOS/Binaries/fitness ns16s`),
+            DrivesBackups::ns16s(),
+            Waves::ns16s(domain),
+            Inbox::ns16s()
+        ]
             .flatten
             .compact
             .select{|item| DoNotShowUntil::isVisible(item["uuid"]) }
             .select{|ns16| InternetStatus::ns16ShouldShow(ns16["uuid"]) }
+    end
+
+    # DisplayListingParameters::removeDuplicates(ns16s)
+    def self.removeDuplicates(ns16s)
+        ns16s.reduce([]){|elements, ns16|
+            if elements.none?{|x| x["uuid"] == ns16["uuid"]} then
+                elements << ns16
+            end
+            elements
+        }
     end
 
     # DisplayListingParameters::getListingParametersForDomain(domain)
@@ -53,12 +44,13 @@ class DisplayListingParameters
     # DisplayListingParameters::getNathalieListingParameters()
     def self.getNathalieListingParameters()
         ns16sPart1 = Domain::domains().map{|domain| DisplayListingParameters::ns16sWithoutNx50s(domain) }.flatten
+        ns16sPart1 = DisplayListingParameters::removeDuplicates(ns16sPart1)
         structure = Nx50s::structureForNathalie()
         {
             "domain"   => nil,
             "monitor"  => structure["Monitor"],
             "overflow" => structure["overflow"],
-            "ns16s"    => ns16sPart1 + structure["Dated"] + structure["Tail"]
+            "ns16s"    => (ns16sPart1 + structure["Dated"] + structure["Tail"]).shuffle
         }
     end
 end
