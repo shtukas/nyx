4d596da29a34b5fb67c61f3bcaa2064a9a190e26
Pascal Honoré <pascal@alseyn.net>
2021-12-02 15:15:39 +0000

    Better drive for listing (eva)

--- Libs/Listings.rb
+++ Libs/Listings.rb
@@ -1,6 +1,9 @@
 
 # encoding: UTF-8
 
+$EvaX1 = 8
+$EvaX2 = 0.4
+
 class Listings
 
     # Listings::listings()
@@ -48,8 +51,7 @@ class Listings
 
         map = {
             "(eva)" => {
-                "type"   => "circuit-breaker",
-                "threshold" => 0.4
+                "type"   => "eva"
             },
             "(work)" => {
                 "type"   => "expectation",
@@ -70,15 +72,18 @@ class Listings
     # Listings::listingToOrderingRatio(listing)
     def self.listingToOrderingRatio(listing)
         driver = Listings::listingDriver(listing)
+        if driver["type"] == "eva" then
+            # Driver eva if for listing (eva)
+            # We have WAVES-UNITS-1-44F7-A64A-72D0205F8957 fed with units
+            # In addition of the bank account "EVA-97F7F3341-4CD1-8B20-4A2466751408"
+            return 1 if Bank::valueOverTimespan("WAVES-UNITS-1-44F7-A64A-72D0205F8957", 3600) > $EvaX1
+            return 1 if Beatrice::stdRecoveredHourlyTimeInHours(Listings::listingToBankAccount(listing)) > $EvaX2
+            return 0
+        end
         if driver["type"] == "expectation" then
             account = Listings::listingToBankAccount(listing)
             target = driver["target"]
             return BankExtended::stdRecoveredDailyTimeInHours(account).to_f/target
-        end
-        if driver["type"] == "circuit-breaker" then
-            account = Listings::listingToBankAccount(listing)
-            threshold = driver["threshold"]
-            return Beatrice::stdRecoveredHourlyTimeInHours(account).to_f/threshold
         end
     end
 
@@ -155,8 +160,10 @@ class Listings
                     if driver["type"] == "expectation" then
                         return "(#{listingToString.call(listing)}: #{(100*ratio).to_i}% of #{driver["target"]} hours)"
                     end
-                    if driver["type"] == "circuit-breaker" then
-                        return "(#{listingToString.call(listing)}: #{(100*ratio).to_i}% of rth #{driver["threshold"]})"
+                    if driver["type"] == "eva" then
+                        v1 = Bank::valueOverTimespan("WAVES-UNITS-1-44F7-A64A-72D0205F8957", 3600)
+                        v2 = Beatrice::stdRecoveredHourlyTimeInHours(Listings::listingToBankAccount(listing))
+                        return "(#{listingToString.call(listing)}: #{v1} of #{$EvaX1} & #{v2.round(2)} of #{$EvaX2})"
                     end
                 }).call(px["listing"], px["ratio"], Listings::listingDriver(px["listing"]))
             }
