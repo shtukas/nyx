d6e03590d6b4a00e209290008dfac9a70aecc9cc
Pascal Honoré <pascal@alseyn.net>
2022-07-08 17:01:32 +0100

    Repair AionTransforms

--- Libs/AionTransforms.rb
+++ Libs/AionTransforms.rb
@@ -5,51 +5,39 @@ class AionTransforms
         AionCore::getAionObjectByHash(operator, rootnhash)["name"]
     end
 
-    # AionTransforms::rewriteThisAionRootWithNewTopName(operator, rootnhash, name1)
-    def self.rewriteThisAionRootWithNewTopName(operator, rootnhash, name1)
+    # AionTransforms::extractDottedExtensionOrNull(str)
+    def self.extractDottedExtensionOrNull(str)
+        extension = File.extname(str)
+        return nil if extension == ".)"
+        extension
+    end
 
-        # name1 can have an extension
+    # AionTransforms::decideLocationName(aionRootName, desiredName) 
+    def self.decideLocationName(aionRootName, desiredName) 
+        aionRootNameExtension = AionTransforms::extractDottedExtensionOrNull(aionRootName)
+        desiredNameExtension = AionTransforms::extractDottedExtensionOrNull(desiredName)
+        if aionRootNameExtension and desiredNameExtension and aionRootNameExtension != desiredNameExtension then
+            puts "AionTransforms::decideLocationName(aionRootName, desiredName)"
+            puts "aionRootName: #{aionRootName}, aionRootNameExtension: #{aionRootNameExtension}"
+            puts "desiredName:  #{desiredName}, desiredNameExtension: #{desiredNameExtension}"
+            raise "(error: 4da4407a-08c9-4bc5-bab9-b99224bed1de)"
+        end
+        if aionRootNameExtension and desiredNameExtension and aionRootNameExtension == desiredNameExtension then
+            return desiredName
+        end
+        if aionRootNameExtension and desiredNameExtension.nil? then
+            return "#{desiredName}#{aionRootNameExtension}"
+        end
+        puts "AionTransforms::decideLocationName(aionRootName, desiredName)"
+        puts "aionRootName: #{aionRootName}, aionRootNameExtension: #{aionRootNameExtension}"
+        puts "desiredName:  #{desiredName}, desiredNameExtension: #{desiredNameExtension}"
+        raise "(error: 5857269b-ac3d-40b2-8a89-3023043e8355)"
+    end
 
+    # AionTransforms::rewriteThisAionRootWithNewTopName(operator, rootnhash, desiredName)
+    def self.rewriteThisAionRootWithNewTopName(operator, rootnhash, desiredName)
         aionObject = AionCore::getAionObjectByHash(operator, rootnhash)
-        name2 = aionObject["name"]
-
-        # name2 can also have an extension.
-
-        # name1 : name we want
-        # name2 : name we have, possibly with an .extension
-
-        namef = (lambda{|name1, name2|
-
-            aBetterExtension = lambda {|na|
-                if na.size < 10 then
-                    return File.extname(na)
-                end
-                File.extname(na[-10, 10])
-            }
-
-            if aBetterExtension.call(name1) != "" and aBetterExtension.call(name2) != "" and aBetterExtension.call(name1) == aBetterExtension.call(name2) then
-                return name1
-            end
-            if aBetterExtension.call(name1) != "" and aBetterExtension.call(name2) != "" and aBetterExtension.call(name1) != aBetterExtension.call(name2) then
-                puts "name1: #{name1}"
-                puts "name2: #{name2}"
-                raise "(error: 80102c05-5181-44be-9b45-8a0b91ebb67b)"
-            end
-            if aBetterExtension.call(name1) != "" and aBetterExtension.call(name2) == "" then
-                puts "name1: #{name1}"
-                puts "name2: #{name2}"
-                raise "(error: b9ed4f40-e3e8-4c78-98e6-d8e04ac62349)"
-            end
-            if aBetterExtension.call(name1) == "" and aBetterExtension.call(name2) != "" then
-                return "#{name1}#{File.extname(name2)}"
-            end
-            if aBetterExtension.call(name1) == "" and aBetterExtension.call(name2) == "" then
-                return name1
-            end
-        }).call(name1, name2)
-
-        aionObject["name"] = namef
-
+        aionObject["name"] = AionTransforms::decideLocationName(aionObject["name"], desiredName) 
         blob = JSON.generate(aionObject)
         operator.putBlob(blob)
     end
