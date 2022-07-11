3a083fedd9a91dca6e48585fd9f978b2defc6b5f
Pascal Honoré <pascal@alseyn.net>
2022-07-11 19:34:00 +0100

    Update in Dx8UnitsUtils

--- Libs/Dx8UnitsUtils.rb
+++ Libs/Dx8UnitsUtils.rb
@@ -10,10 +10,6 @@ class Dx8UnitsUtils
     # Dx8UnitsUtils::acquireUnit(dx8UnitId)
     def self.acquireUnit(dx8UnitId) # returns the location of the unit, or nil if it could not be acquired
 
-        location1 = XCache::filepath("213a1c6e-df37-46ed-95b2-15ef742c5512:#{dx8UnitId}")
-        return location1 if File.exists?(location1)
-
-        # We could not find the file in xcache, now looking in stargate central
         status = StargateCentral::askForInfinityReturnBoolean()
         if !status then
             puts "Could not access dx8UnitId #{dx8UnitId}"
