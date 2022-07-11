8476eec61feee510d777932ce84744563594d28c
Pascal Honoré <pascal@alseyn.net>
2022-07-11 21:09:56 +0100

    Final specs for Fx18 Files

---
+++ Libs/Fx18.rb
@@ -0,0 +1,56 @@
+
+class Fx18
+
+    # --------------------------------------------------------------
+
+    # Fx18::conputeLocalFilepathForObjectUUID(objectuuid)
+    def self.conputeLocalFilepathForObjectUUID(objectuuid)
+        "#{Config::pathToDataBankStargate()}/Fx18s"
+    end
+
+    # Fx18::constructNewFile(objectuuid) # location (That function constructs the database and create the _events_ table)
+    def self.constructNewFile(objectuuid)
+        
+    end
+
+    # --------------------------------------------------------------
+
+    # Fx18::setAttribute(eventuuid, eventTime, objectuuid, attname, attvalue)
+    def self.setAttribute(eventuuid, eventTime, objectuuid, attname, attvalue)
+        
+    end
+
+    # Fx18::getAttributeOrNull(objectuuid, attname)
+    def self.getAttributeOrNull(objectuuid, attname)
+        
+    end
+
+    # --------------------------------------------------------------
+
+    # Fx18::setsAdd(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value)
+    def self.setsAdd(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value)
+        
+    end
+
+    # Fx18::setsRemove(eventuuid, eventTime, objectuuid, setuuid, itemuuid)
+    def self.setsRemove(eventuuid, eventTime, objectuuid, setuuid, itemuuid)
+        
+    end
+
+    # Fx18::setsItems(objectuuid, setuuid)
+    def self.setsItems(objectuuid, setuuid)
+        
+    end
+
+    # --------------------------------------------------------------
+
+    # Fx18::putData(eventuuid, eventTime, objectuuid, key, data)
+    def self.putData(eventuuid, eventTime, objectuuid, key, data)
+        
+    end
+
+    # Fx18::getDataOrNull(objectuuid, key)
+    def self.getDataOrNull(objectuuid, key)
+        
+    end
+end
