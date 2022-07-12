68c8cf9113c007df78c0d4dbadfc7ed0ab1aa246
Pascal Honoré <pascal@alseyn.net>
2022-07-12 03:46:46 +0100

    Fx18s updates

--- Libs/Fx18s.rb
+++ Libs/Fx18s.rb
@@ -11,6 +11,12 @@ class Fx18s
     # Fx18s::constructNewFile(objectuuid) # location (That function constructs the database and creates the _fx18_ table)
     def self.constructNewFile(objectuuid)
         filepath = Fx18s::computeLocalFx18Filepath(objectuuid)
+        if File.exists?(filepath) then
+            puts "operation: Fx18s::constructNewFile"
+            puts "objectuuid: #{objectuuid}"
+            puts "filepath: #{filepath}"
+            raise "(error: a906e951-c38e-4b96-a6b4-0084df2ff854) file already exists"
+        end
         db = SQLite3::Database.new(filepath)
         db.busy_timeout = 117
         db.busy_handler { |count| true }
@@ -19,64 +25,75 @@ class Fx18s
         db.close
     end
 
-    # --------------------------------------------------------------
-
-    # Fx18s::setAttribute(eventuuid, eventTime, objectuuid, attname, attvalue)
-    def self.setAttribute(eventuuid, eventTime, objectuuid, attname, attvalue)
-        
-    end
-
-    # Fx18s::setAttribute2(objectuuid, attname, attvalue)
-    def self.setAttribute2(objectuuid, attname, attvalue)
-        
-    end
-
-    # Fx18s::getAttributeOrNull(objectuuid, attname)
-    def self.getAttributeOrNull(objectuuid, attname)
-        
+    # Fx18s::acquireFilepath(objectuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.acquireFilepath(objectuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+        Fx18s::computeLocalFx18Filepath(objectuuid)
     end
 
     # --------------------------------------------------------------
 
-    # Fx18s::setsAdd(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value)
-    def self.setsAdd(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value)
-        
+    # Fx18s::setAttribute1(eventuuid, eventTime, objectuuid, attname, attvalue, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setAttribute1(eventuuid, eventTime, objectuuid, attname, attvalue, shouldDownloadFileIfFoundOnRemoteDrive)
+        filepath = Fx18s::acquireFilepath(objectuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+        db = SQLite3::Database.new(filepath)
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.execute "delete from _fx18_ where _eventuuid_=?", [eventuuid]
+        db.execute "insert into _fx18_ (_eventuuid_, _eventTime_, _eventData1_, _eventData2_, _eventData3_) values (?, ?, ?, ?, ?)", [eventuuid, eventTime, "attribute", attname, attvalue]
+        db.close
     end
 
-    # Fx18s::setsAdd2(objectuuid, setuuid, itemuuid, value)
-    def self.setsAdd2(objectuuid, setuuid, itemuuid, value)
-        
+    # Fx18s::setAttribute2(objectuuid, attname, attvalue, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setAttribute2(objectuuid, attname, attvalue, shouldDownloadFileIfFoundOnRemoteDrive)
+        Fx18s::setAttribute1(SecureRandom.uuid, Time.new.to_f, objectuuid, attname, attvalue, shouldDownloadFileIfFoundOnRemoteDrive)
     end
 
-    # Fx18s::setsRemove(eventuuid, eventTime, objectuuid, setuuid, itemuuid)
-    def self.setsRemove(eventuuid, eventTime, objectuuid, setuuid, itemuuid)
-        
-    end
-
-    # Fx18s::setsRemove2(objectuuid, setuuid, itemuuid)
-    def self.setsRemove2(objectuuid, setuuid, itemuuid)
-        
-    end
-
-    # Fx18s::setsItems(objectuuid, setuuid)
-    def self.setsItems(objectuuid, setuuid)
+    # Fx18s::getAttributeOrNull(objectuuid, attname, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.getAttributeOrNull(objectuuid, attname, shouldDownloadFileIfFoundOnRemoteDrive)
         
     end
 
     # --------------------------------------------------------------
 
-    # Fx18s::putData(eventuuid, eventTime, objectuuid, key, data)
-    def self.putData(eventuuid, eventTime, objectuuid, key, data)
+    # Fx18s::setsAdd1(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setsAdd1(eventuuid, eventTime, objectuuid, setuuid, itemuuid, value, shouldDownloadFileIfFoundOnRemoteDrive)
         
     end
 
-    # Fx18s::putData2(objectuuid, key, data)
-    def self.putData2(objectuuid, key, data)
+    # Fx18s::setsAdd2(objectuuid, setuuid, itemuuid, value, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setsAdd2(objectuuid, setuuid, itemuuid, value, shouldDownloadFileIfFoundOnRemoteDrive)
         
     end
 
-    # Fx18s::getDataOrNull(objectuuid, key)
-    def self.getDataOrNull(objectuuid, key)
+    # Fx18s::setsRemove1(eventuuid, eventTime, objectuuid, setuuid, itemuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setsRemove1(eventuuid, eventTime, objectuuid, setuuid, itemuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+        
+    end
+
+    # Fx18s::setsRemove2(objectuuid, setuuid, itemuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setsRemove2(objectuuid, setuuid, itemuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+        
+    end
+
+    # Fx18s::setsItems(objectuuid, setuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.setsItems(objectuuid, setuuid, shouldDownloadFileIfFoundOnRemoteDrive)
+        
+    end
+
+    # --------------------------------------------------------------
+
+    # Fx18s::putData1(eventuuid, eventTime, objectuuid, key, data, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.putData1(eventuuid, eventTime, objectuuid, key, data, shouldDownloadFileIfFoundOnRemoteDrive)
+        
+    end
+
+    # Fx18s::putData2(objectuuid, key, data, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.putData2(objectuuid, key, data, shouldDownloadFileIfFoundOnRemoteDrive)
+        
+    end
+
+    # Fx18s::getDataOrNull(objectuuid, key, shouldDownloadFileIfFoundOnRemoteDrive)
+    def self.getDataOrNull(objectuuid, key, shouldDownloadFileIfFoundOnRemoteDrive)
         
     end
 end
