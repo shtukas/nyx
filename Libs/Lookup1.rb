15c1d18ac5ab92b397280cfa7077fdd139ffc064
Pascal Honoré <pascal@alseyn.net>
2022-07-28 22:22:32 +0100

    Lookup1 improvments

--- Libs/Lookup1.rb
+++ Libs/Lookup1.rb
@@ -3,7 +3,7 @@ class Lookup1
 
     # Lookup1::getDatabaseFilepath()
     def self.getDatabaseFilepath()
-        filepath = XCache::filepath("#{Fx18::cachePrefix()}:4f8266b4-dc36-4699-a592-68eee2c3ac69")
+        filepath = XCache::filepath("#{Fx18::cachePrefix()}:4f8266b4-dc37-4699-a592-68eee2c3ac69")
         return filepath if File.exists?(filepath)
 
         puts "preparing lookup1 database: #{filepath}"
@@ -11,10 +11,42 @@ class Lookup1
         db.busy_timeout = 117
         db.busy_handler { |count| true }
         db.results_as_hash = true
-        db.execute("create table _lookup1_ (_itemuuid_ text primary key, _unixtime_ float, _mikuType_ text, _item_ text, _announce_ text)", [])
+        db.execute("create table _lookup1_ (_itemuuid_ text primary key, _unixtime_ float, _mikuType_ text, _item_ text, _description_ text)", [])
         db.close
 
         filepath
+    end
+
+    # Lookup1::commit(objectuuid, unixtime, mikuType, item, description)
+    def self.commit(objectuuid, unixtime, mikuType, item, description)
+        db = SQLite3::Database.new(Lookup1::getDatabaseFilepath())
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        db.execute("delete from _lookup1_ where _itemuuid_=?", [objectuuid])
+        db.execute("insert into _lookup1_ (_itemuuid_, _unixtime_, _mikuType_, _item_, _description_) values (?, ?, ?, ?, ?)", [objectuuid, unixtime, mikuType, JSON.generate(item), description])
+        db.close
+    end
+
+    # Lookup1::getElementsOrNull(objectuuid)
+    def self.getElementsOrNull(objectuuid)
+        unixtime = Fx18Attributes::getOrNull(objectuuid, "unixtime")
+        return nil if unixtime.nil?
+        mikuType = Fx18Attributes::getOrNull(objectuuid, "mikuType")
+        return nil if mikuType.nil?
+        item     = Fx18::itemOrNull(objectuuid)
+        return nil if item.nil?
+        description = LxFunction::function("generic-description", item)
+        [objectuuid, unixtime, mikuType, item, description]
+    end
+
+    # Lookup1::processObjectuuid(objectuuid)
+    def self.processObjectuuid(objectuuid)
+        elements = Lookup1::getElementsOrNull(objectuuid)
+        return if elements.nil?
+        objectuuid, unixtime, mikuType, item, description = elements
+        puts "update lookup1: objectuuid: #{objectuuid}"
+        Lookup1::commit(objectuuid, unixtime, mikuType, item, description)
     end
 
     # Lookup1::itemsuuids()
@@ -80,9 +112,9 @@ class Lookup1
         db.busy_handler { |count| true }
         db.results_as_hash = true
         nx20s = []
-        db.execute("select _itemuuid_, _unixtime_, _announce_ from _lookup1_", []) do |row|
+        db.execute("select _itemuuid_, _unixtime_, _description_ from _lookup1_", []) do |row|
             nx20s << {
-                "announce"   => row["_announce_"],
+                "announce"   => row["_description_"],
                 "unixtime"   => row["_unixtime_"],
                 "objectuuid" => row["_itemuuid_"]
             }
@@ -95,29 +127,7 @@ class Lookup1
     def self.processEventInternally(event)
         if event["mikuType"] == "(object has been updated)" then
             objectuuid = event["objectuuid"]
-
-            # (comment 1) This happens naturally if this was trigerred by a stargate sync and the object is only partially recorded in the local block
-
-            unixtime = Fx18Attributes::getOrNull(objectuuid, "unixtime")
-            return if unixtime.nil? # (comment 1)
-
-            mikuType = Fx18Attributes::getOrNull(objectuuid, "mikuType")
-            return if mikuType.nil? # (comment 1)
-
-            item     = Fx18::itemOrNull(objectuuid)
-            return if item.nil? # (comment 1)
-
-            puts "update lookup1: objectuuid: #{objectuuid}"
-
-            announce = LxFunction::function("generic-description", item)
-
-            db = SQLite3::Database.new(Lookup1::getDatabaseFilepath())
-            db.busy_timeout = 117
-            db.busy_handler { |count| true }
-            db.results_as_hash = true
-            db.execute("delete from _lookup1_ where _itemuuid_=?", [objectuuid])
-            db.execute("insert into _lookup1_ (_itemuuid_, _unixtime_, _mikuType_, _item_, _announce_) values (?, ?, ?, ?, ?)", [objectuuid, unixtime, mikuType, JSON.generate(item), announce])
-            db.close
+            Lookup1::processObjectuuid(objectuuid)
         end
 
         if event["mikuType"] == "(object has been deleted)" then
@@ -135,27 +145,6 @@ class Lookup1
     def self.maintainLookup()
         (Fx18::objectuuids() - Lookup1::itemsuuids())
             .first(500)
-            .each{|objectuuid|
-                puts "building lookup1: objectuuid: #{objectuuid}"
-
-                unixtime = Fx18Attributes::getOrNull(objectuuid, "unixtime")
-                raise "(error: a9e52d99-1bd5-4b65-9ee6-a3c1b2c9d0cb)" if unixtime.nil?
-
-                mikuType = Fx18Attributes::getOrNull(objectuuid, "mikuType")
-                raise "(error: cd46d88b-ddb7-412f-bbb5-acccf968ba3e)" if mikuType.nil?
-
-                item     = Fx18::itemOrNull(objectuuid)
-                raise "(error: d644d601-288e-426c-9467-3a364a32d06b)" if item.nil?
-
-                announce = LxFunction::function("generic-description", item)
-
-                db = SQLite3::Database.new(Lookup1::getDatabaseFilepath())
-                db.busy_timeout = 117
-                db.busy_handler { |count| true }
-                db.results_as_hash = true
-                db.execute("delete from _lookup1_ where _itemuuid_=?", [objectuuid])
-                db.execute("insert into _lookup1_ (_itemuuid_, _unixtime_, _mikuType_, _item_, _announce_) values (?, ?, ?, ?, ?)", [objectuuid, unixtime, mikuType, JSON.generate(item), announce])
-                db.close
-            }
+            .each{|objectuuid| Lookup1::processObjectuuid(objectuuid) }
     end
 end
