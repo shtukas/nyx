6199676887595fb1bac56d9586378ce53a8b84d7
Pascal Honoré <pascal@alseyn.net>
2022-09-18 19:27:40 +0100

    -

--- Libs/CommandInterpreters.rb
+++ Libs/CommandInterpreters.rb
@@ -6,7 +6,7 @@ class CommandInterpreters
     # CommandInterpreters::catalystListingCommands()
     def self.catalystListingCommands()
         [
-            ".. | <datecode> | <n> | start (<n>) | stop (<n>) | access (<n>) | description (<n>) | name (<n>) | datetime (<n>) | nx112 (<n>) | landing (<n>) | pause (<n>) | pursue (<n>) | do not show until (<n>) | redate (<n>) | done (<n>) | done for today | edit (<n>) | transmute (<n>) | time * * | expose (<n>) | destroy",
+            ".. | <datecode> | <n> | start (<n>) | stop (<n>) | access (<n>) | description (<n>) | name (<n>) | datetime (<n>) | nx112 (<n>) | landing (<n>) | pause (<n>) | pursue (<n>) | do not show until <n> | redate (<n>) | done (<n>) | done for today | edit (<n>) | transmute (<n>) | time * * | expose (<n>) | destroy",
             "update start date (<n>)",
             "wave | anniversary | today | ondate | todo | task | toplevel | inbox | line",
             "anniversaries | ondates | todos | waves | tc",
