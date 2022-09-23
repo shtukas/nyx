f469c83bb6250b9130a4c98fef9b062564a4b368
Pascal Honoré <pascal@alseyn.net>
2022-09-23 07:04:49 +0100

    SystemEvents

--- Libs/SystemEvents.rb
+++ Libs/SystemEvents.rb
@@ -195,6 +195,7 @@ class SystemEvents
                     end
                     db1.close
                     FileUtils.rm(filepath1)
+                    Items::batchUpdateFromEventLog(true)
                     next
                 end
 
