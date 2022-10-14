3eddd8e48bf6a4d2498e0fe5671506832ec5e068
Pascal Honoré <pascal@alseyn.net>
2022-10-14 17:29:49 +0100

    -

--- Libs/loader.rb
+++ Libs/loader.rb
@@ -48,10 +48,9 @@ checkLocation = lambda{|location|
 
 
 checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-Config.json")
-checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-DataCenter/ActiveCommsLine")
+checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-DataCenter/CommsLine")
 checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-DataCenter/DataStore1")
 checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-DataCenter/Instance-Databases")
-checkLocation.call("#{ENV['HOME']}/Galaxy/DataBank/Stargate-DataCenter/StagingCommsLine")
 
 checkLocation.call("#{ENV['HOME']}/Galaxy/DataHub")
 checkLocation.call("#{ENV['HOME']}/Galaxy/DataHub/NxTodos-BufferIn")
