9e0c5b6688028f4744598da49560731e16102a08
Pascal Honoré <pascal@alseyn.net>
2022-10-28 21:31:11 +0100

    gridfire-log

--- Libs/GridFire.rb
+++ Libs/GridFire.rb
@@ -2,6 +2,18 @@
 
 class GridFire
 
+    # GridFire::log(message)
+    def self.log(message)
+        month = Time.new.to_s[0, 7]
+        day   = Time.new.to_s[0, 10]
+        filepath = "#{Config::userHomeDirectory()}/Galaxy/DataBank/Stargate-DataCenter/gridfire-log/#{month}/#{day}.txt"
+        if !File.exists?(File.dirname(filepath)) then
+            FileUtils.mkpath(File.dirname(filepath))
+        end
+        line = "GridFire @ #{Time.new.utc.iso8601}: #{message}"
+        File.open(filepath, "a"){|f| f.puts(line) }
+    end
+
     # GridFire::orderStates(states)
     def self.orderStates(states)
         states.sort{|s1, s2| s1["unixtime"] <=> s2["unixtime"] }
@@ -28,8 +40,8 @@ class GridFire
                 location1.gsub(".NxGridPointN", "")
             end
         if !File.exists?(location2) then
-            puts "GridFire: I am trying to get the export folder for object '#{location1}'"
-            puts "GridFire: but I can't find it."
+            GridFire::log "I am trying to get the export folder for object '#{location1}'"
+            GridFire::log "but I can't find it."
         end
         location2
     end
@@ -49,7 +61,7 @@ class GridFire
         Find.find("#{Config::userHomeDirectory()}/Galaxy/OpenCycles") do |path|
             if path[-12, 12] == "NxGridPointN" then
                 location1 =  path
-                puts "GridFire: probing #{location1}"
+                GridFire::log "probing #{location1}"
                 fsObject = JSON.parse(IO.read(location1))
                 uuid = fsObject["uuid"]
                 location2 = GridFire::objectLocationToExportFolder(location1)
@@ -59,7 +71,7 @@ class GridFire
                 cachedTrace = GridFire::getCachedTraceOrNull(location1, fsObject["states"].last["uuid"])
                 shouldMakeNewState = (cachedTrace.nil? or (currentTrace != cachedTrace))
                 if shouldMakeNewState then
-                    puts "GridFire: about to make new state from: #{location2}".green
+                    GridFire::log "about to make new state from: #{location2}".green
                     state = GridState::directoryPathToNxDirectoryContentsGridState(location2)
                     #puts JSON.pretty_generate(state)
                     fsObject["states"] << state
@@ -75,34 +87,34 @@ class GridFire
         Find.find("#{Config::userHomeDirectory()}/Galaxy/OpenCycles") do |path|
             if path[-12, 12] == "NxGridPointN" then
                 location1 =  path
-                puts "GridFire: probing #{location1}"
+                GridFire::log "probing #{location1}"
                 fsObject = JSON.parse(IO.read(location1))
                 uuid = fsObject["uuid"]
                 # The first thing we do is to sync with network
                 networkObject = NxGridPointN::getItemOrNull(uuid)
                 if networkObject.nil? then
-                    puts "GridFire: I could not find the network equivalent of location1: #{location1}"
-                    puts "GridFire: Exiting"
+                    GridFire::log "I could not find the network equivalent of location1: #{location1}"
+                    GridFire::log "Exiting"
                     exit
                 end
                 combinedStates = GridFire::combineStates(fsObject["states"], networkObject["states"])
                 if combinedStates.size > networkObject["states"].size then
-                    puts "GridFire: sending updated object to network".green
+                    GridFire::log "sending updated object to network".green
                     networkObject["states"] = combinedStates
                     NxGridPointN::commitObject(networkObject)
                 end
                 if combinedStates.size > fsObject["states"].size then
-                    puts "GridFire: sending updated object to disk".green
+                    GridFire::log "sending updated object to disk".green
                     if combinedStates.last["type"] != "NxDirectoryContents" then
-                        puts "GridFire: The updated last state of object here: #{location1}"
-                        puts "GridFire: is not a NxDirectoryContents"
-                        puts "GridFire: Exiting"
+                        GridFire::log "The updated last state of object here: #{location1}"
+                        GridFire::log "is not a NxDirectoryContents"
+                        GridFire::log "Exiting"
                         exit
                     end
                     fsObject["states"] = combinedStates
                     File.open(location1, "w"){|f| f.puts(JSON.pretty_generate(fsObject)) }
                     location2 = GridFire::objectLocationToExportFolder(location1)
-                    puts "GridFire: exporting the last state to: #{location2}".green
+                    GridFire::log "exporting the last state to: #{location2}".green
                     state = fsObject["states"].last
                     GridState::exportNxDirectoryContentsRootsAtFolder(state["rootnhashes"], location2)
                     GridFire::setTrace(location1, state["uuid"], CommonUtils::locationTrace(location2))
