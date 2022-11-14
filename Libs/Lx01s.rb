d375dee736e875715b2ecb4a65dc296e0ed27ed7
Pascal Honoré <pascal@alseyn.net>
2022-11-14 10:57:55 +0000

    Girdfire and network management updates

--- Libs/Lx01s.rb
+++ Libs/Lx01s.rb
@@ -16,11 +16,11 @@ class Lx01s
     def self.listingItems()
         items = []
 
-        unixtime = XCache::getOrDefaultValue("541c04ca-f456-435d-b33b-6ea6f3059f6e", "0").to_f
+        unixtime = XCache::getOrDefaultValue("network-maintenance-time-aa34c002-96b1-43b4-a7fd-275af066150f", "0").to_f
         if (Time.new.to_i - unixtime) > 3600*8 then
             items << Lx01s::make("ED7B8BE3-755B-40F9-AEEE-02790FD7C952", "AutomaticNx7NetworkMainteance::run()", lambda{
                 AutomaticNx7NetworkMainteance::run()
-                XCache::set("541c04ca-f456-435d-b33b-6ea6f3059f6e", Time.new.to_i)
+                XCache::set("network-maintenance-time-aa34c002-96b1-43b4-a7fd-275af066150f", Time.new.to_i)
             })
         end
 
