1b4fdf9431f33ae907ef0092f052828f65e547ba
Pascal Honoré <pascal@alseyn.net>
2022-12-08 08:46:48 +0000

    cleaning

--- Libs/loader.rb
+++ Libs/loader.rb
@@ -184,7 +184,6 @@ require_relative "Nx7Payloads.rb"
 require_relative "NxTriages.rb"
 require_relative "NxOndates.rb"
 require_relative "NxBalls.rb"
-require_relative "NyxDatablobs.rb"
 
 require_relative "PrimitiveFiles.rb"
 require_relative "ProgrammableBooleans.rb"
