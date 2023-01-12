63f168e3fd1630f9141354970c7ad26e9f0df3dd
Pascal Honoré <pascal@alseyn.net>
2023-01-12 19:03:48 +0000

    doNotShowUntil content addressable

--- Libs/DoNotShowUntil.rb
+++ Libs/DoNotShowUntil.rb
@@ -2,19 +2,80 @@
 
 # create table _dnsu_ (_uuid_ text, _unixtime_ float);
 
+class DNSUIO
+
+    # DNSUIO::filepaths()
+    def self.filepaths()
+        LucilleCore::locationsAtFolder("#{Config::pathToDataCenter()}/DoNotShowUntil2")
+            .select{|filepath| filepath[-5, 5] == ".json" }
+    end
+
+    # DNSUIO::filepathsForUuid(uuid) # filepaths for this uuid (possibly empty)
+    def self.filepathsForUuid(uuid)
+        DNSUIO::filepaths()
+            .select{|filepath|
+                data = JSON.parse(IO.read(filepath))
+                data["uuid"] == uuid
+            }
+    end
+
+    # DNSUIO::unixtimeOrNull(uuid)
+    def self.unixtimeOrNull(uuid)
+
+        key1 = "26f5a1eb-3568-4e77-87a9-b29d56732fbd:#{DNSUIO::filepaths().join(":")}:#{uuid}"
+        unixtime = XCache::getOrNull(key1)
+        return unixtime.to_f if unixtime
+
+        unixtime = DNSUIO::filepathsForUuid(uuid).reduce(nil){|m, u|
+            u = JSON.parse(IO.read(u))["unixtime"]
+            if m then
+                [m, u].max
+            else
+                u
+            end
+        }
+
+        if unixtime then
+            XCache::set(key1, unixtime)
+        end
+
+        unixtime
+    end
+
+    # DNSUIO::setUnixtime(uuid, unixtime)
+    def self.setUnixtime(uuid, unixtime)
+        DNSUIO::filepathsForUuid(uuid)
+            .each{|filepath| FileUtils.rm(filepath)}
+
+        data = {
+            "uuid"     => uuid,
+            "unixtime" => unixtime,
+            "random"   => SecureRandom.hex
+        }
+        contents = JSON.generate(data)
+        filename = "#{Digest::SHA1.hexdigest(contents)}.json"
+        filepath = "#{Config::pathToDataCenter()}/DoNotShowUntil2/#{filename}"
+        File.open(filepath, "w") {|f| f.puts(contents) }
+    end
+
+end
+
 class DoNotShowUntil
 
     # DoNotShowUntil::setUnixtime(uuid, unixtime)
     def self.setUnixtime(uuid, unixtime)
-        filepath = "#{Config::pathToDataCenter()}/DoNotShowUntil/#{uuid}.info"
-        File.open(filepath, "w"){|f| f.puts(unixtime) }
+        DNSUIO::setUnixtime(uuid, unixtime)
     end
 
     # DoNotShowUntil::getUnixtimeOrNull(uuid)
     def self.getUnixtimeOrNull(uuid)
         filepath = "#{Config::pathToDataCenter()}/DoNotShowUntil/#{uuid}.info"
-        return nil if !File.exists?(filepath)
-        IO.read(filepath).strip.to_f
+        if File.exists?(filepath) then
+            unixtime = IO.read(filepath).strip.to_f
+            DNSUIO::setUnixtime(uuid, unixtime)
+            FileUtils.rm(filepath)
+        end
+        DNSUIO::unixtimeOrNull(uuid)
     end
 
     # DoNotShowUntil::getDateTimeOrNull(uuid)
