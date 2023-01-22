684ba206fad13f7d8fdf678984fa17792cffeb88
Pascal Honoré <pascal@alseyn.net>
2023-01-22 17:44:57 +0000

    Database1 update

--- Libs/Database1.rb
+++ Libs/Database1.rb
@@ -1,8 +1,20 @@
 
 # Database
-# create table objects (uuid text, mikuType text, unixtime float, datetime text, description text, tcId text, doNotShowUntil float, skippedUntil float, field1 text, field2 text, field3 text, field4 text, field5 text, field6 text, field7 text)
+# create table objects (uuid text, mikuType text, unixtime float, datetime text, description text, doNotShowUntil float, field1 text, field2 text, field3 text, field4 text, field5 text, field6 text, field7 text, field8 text, field9 text);
 
 class Database1
+
+    # Database1::spawnNewDatabase()
+    def self.spawnNewDatabase()
+        filepath = "#{Config::pathToDataCenter()}/Database1/#{CommonUtils::timeStringL22()}.sqlite3"
+        db = SQLite3::Database.new(filepath)
+        db.busy_timeout = 117
+        db.busy_handler { |count| true }
+        db.results_as_hash = true
+        db.execute("create table objects (uuid text, mikuType text, unixtime float, datetime text, description text, doNotShowUntil float, field1 text, field2 text, field3 text, field4 text, field5 text, field6 text, field7 text, field8 text, field9 text);", [])
+        db.close
+        filepath
+    end
 
     # Database1::getObjectByUUIDOrNull(uuid)
     def self.getObjectByUUIDOrNull(uuid)
