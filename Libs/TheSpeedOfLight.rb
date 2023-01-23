46ce373e7671d27b66d46844bcfa09fb39544987
Pascal Honoré <pascal@alseyn.net>
2023-01-23 07:20:07 +0000

    Avoid speed of light file conflicts

--- Libs/TheSpeedOfLight.rb
+++ Libs/TheSpeedOfLight.rb
@@ -1,14 +1,29 @@
 
 class TheSpeedOfLight
- 
+
+    # TheSpeedOfLight::getFilepath()
+    def self.getFilepath()
+        LucilleCore::locationsAtFolder("#{Config::pathToDataCenter()}/TheSpeedOfLight")
+            .select{|filepath| filepath[-5, 5] == ".json" }
+            .first
+    end
+
     # TheSpeedOfLight::getDaySpeedOfLight()
     def self.getDaySpeedOfLight()
-        filepath = "#{Config::pathToDataCenter()}/TheSpeedOfLight.json"
+        filepath = TheSpeedOfLight::getFilepath()
         return 1 if !File.exists?(filepath)
         data = JSON.parse(IO.read(filepath))
         # data: {date, value}
         return 1 if data["date"] != CommonUtils::today()
         data["speed"]
+    end
+
+    # TheSpeedOfLight::putData(data)
+    def self.putData(data)
+        filepath1 = TheSpeedOfLight::getFilepath()
+        filepath2 = "#{Config::pathToDataCenter()}/TheSpeedOfLight/#{CommonUtils::timeStringL22()}.json"
+        File.open(filepath2, "w"){|f| f.puts(JSON.pretty_generate(data)) }
+        FileUtils.rm(filepath1)
     end
 
     # TheSpeedOfLight::decrementLightSpeed()
@@ -18,8 +33,7 @@ class TheSpeedOfLight
             "date"  => CommonUtils::today(),
             "speed" => [speed-0.1*rand, 0].max
         }
-        filepath = "#{Config::pathToDataCenter()}/TheSpeedOfLight.json"
-        File.open(filepath, "w"){|f| f.puts(JSON.pretty_generate(data)) }
+        TheSpeedOfLight::putData(data)
     end
  
     # TheSpeedOfLight::incrementLightSpeed()
@@ -29,7 +43,6 @@ class TheSpeedOfLight
             "date"  => CommonUtils::today(),
             "speed" => speed+0.1*rand
         }
-        filepath = "#{Config::pathToDataCenter()}/TheSpeedOfLight.json"
-        File.open(filepath, "w"){|f| f.puts(JSON.pretty_generate(data)) }
+        TheSpeedOfLight::putData(data)
     end
 end
