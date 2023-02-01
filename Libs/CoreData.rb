a21cbeeb19b146a54a9230d8ca6e04ea018c5ef6
Pascal Honoré <pascal@alseyn.net>
2023-02-01 15:17:06 +0000

    CoreData update

--- Libs/CoreData.rb
+++ Libs/CoreData.rb
@@ -5,7 +5,7 @@ class CoreData
 
     # CoreData::coreDataReferenceTypes()
     def self.coreDataReferenceTypes()
-        ["nyx-directory"]
+        ["nyx directory", "unique string"]
     end
 
     # CoreData::interactivelySelectCoreDataReferenceType()
@@ -19,12 +19,22 @@ class CoreData
         # This function is called during the making of a new node (or when we are issuing a new payload of an existing node)
         # It does stuff and returns a payload string or null
         referencetype = CoreData::interactivelySelectCoreDataReferenceType()
-        return nil if referencetype.nil?
-        if referencetype == "nyx-directory" then
+        if referencetype.nil? then
+            if LucilleCore::askQuestionAnswerAsBoolean("> confirm null reference string ? ") then
+                return "null"
+            else
+                return CoreData::referenceString(uuid)
+            end
+        end
+        if referencetype == "nyx directory" then
             folderpath = NyxDirectories::makeNewDirectory(uuid)
             system("open '#{folderpath}'")
             LucilleCore::pressEnterToContinue()
             return "nyx-directory:#{uuid}"
+        end
+        if referencetype == "unique string" then
+            uniquestring = LucilleCore::askQuestionAnswerAsString("unique string: ")
+            return "unique-string:#{uniquestring}"
         end
         raise "(error: f75b2797-99e5-49d0-8d49-40b44beb538c) unsupported core data reference type: #{referencetype}"
     end
