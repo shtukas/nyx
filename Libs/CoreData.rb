7427f478eff60ce8aa29ceb5ebbcf1870cb8be96
Pascal Honoré <pascal@alseyn.net>
2023-02-01 11:03:25 +0000

    CoreData (2)

--- Libs/CoreData.rb
+++ Libs/CoreData.rb
@@ -7,14 +7,14 @@ class CoreData
     # a single string and mutations of the storage unit are transparent to the holder.
     # Issuing another storage unit corresponds to a different string
 
-    # CoreData::payloadTypes()
+    # CoreData::coreDataTypes()
     def self.payloadTypes()
         ["nyx-directory"]
     end
 
-    # CoreData::interactivelySelectNyxPayloadType()
-    def self.interactivelySelectNyxPayloadType()
-        types = CoreData::payloadTypes()
+    # CoreData::interactivelySelectCoreDataType()
+    def self.interactivelySelectCoreDataType()
+        types = CoreData::coreDataTypes()
         LucilleCore::selectEntityFromListOfEntitiesOrNull("coredata type", types)
     end
 
@@ -22,15 +22,15 @@ class CoreData
     def self.issuePayload(uuid)
         # This function is called during the making of a new node (or when we are issuing a new payload of an existing node)
         # It does stuff and returns a payload string or null
-        payloadType = CoreData::interactivelySelectNyxPayloadType()
+        payloadType = CoreData::interactivelySelectCoreDataType()
         return nil if payloadType.nil?
         if payloadType == "nyx-directory" then
             folderpath = NyxDirectories::makeNewDirectory(uuid)
             system("open '#{folderpath}'")
             LucilleCore::pressEnterToContinue()
-            return "nyx-directory" # The first nyx-directory is a payload type, and the second is a full payload string
+            return "nyx-directory:#{uuid}"
         end
-        raise "(error: f75b2797-99e5-49d0-8d49-40b44beb538c) unsupported payload type: #{payloadType}"
+        raise "(error: f75b2797-99e5-49d0-8d49-40b44beb538c) unsupported coredata type: #{payloadType}"
     end
 
 end
