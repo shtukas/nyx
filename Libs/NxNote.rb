eda7fe325d71838d3e7c25a17bb6478b39498bc7
Pascal Honoré <pascal@alseyn.net>
2023-04-03 09:16:57 +0100

    sync

--- Libs/NxNote.rb
+++ Libs/NxNote.rb
@@ -1,20 +1,33 @@
 
 class NxNote
 
-    # NxNote::makeNoteOrNull()
-    def self.makeNoteOrNull()
-        line = LucilleCore::askQuestionAnswerAsString("line: ")
-        return nil if line == ""
-        {
-            "uuid"     => SecureRandom.uuid,
-            "unixtime" => Time.new.to_i,
-            "line"     => line
-        }
+    # NxNote::items()
+    def self.items()
+        N3Objects::getMikuType("NxNote")
     end
 
-    # NxNote::landing(note)
-    def self.landing(note)
-        puts "NxNote::landing not implemented yet"
-        LucilleCore::pressEnterToContinue() 
+    # NxNote::commit(item)
+    def self.commit(item)
+        N3Objects::commit(item)
+    end
+
+    # NxNote::destroy(uuid)
+    def self.destroy(uuid)
+        N3Objects::destroy(uuid)
+    end
+
+    # NxNote::issue(line)
+    def self.issue(line)
+        uuid  = SecureRandom.uuid
+        item = {
+            "uuid"        => uuid,
+            "mikuType"    => "NxNote",
+            "unixtime"    => Time.new.to_i,
+            "datetime"    => Time.new.utc.iso8601,
+            "description" => line
+        }
+        puts JSON.pretty_generate(item)
+        NxNote::commit(item)
+        item
     end
 end
