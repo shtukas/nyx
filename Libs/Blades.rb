0abba56ace853851d098047d16b83a61034f6121
Pascal Honoré <pascal@alseyn.net>
2025-12-26 21:24:50 +0000

    blades

--- Libs/Blades.rb
+++ Libs/Blades.rb
@@ -23,6 +23,11 @@ class BladesConfig
     def self.repository_path()
         "#{Config::pathToNyxData()}/blades"
     end
+
+    # BladesConfig::cache_prefix()
+    def self.cache_prefix()
+        "d5b50dac-4562-4189-8022-b47d9c011c3d"
+    end
 end
 
 class Blades
@@ -126,7 +131,9 @@ class Blades
     # --------------------------------------------------------------------------
 
     # --------------------------------------------------------------------------
-    # Items
+    # Public interface
+
+    @memory1 = {}
 
     # Blades::init(uuid, mikuType)
     def self.init(uuid, mikuType)
@@ -183,6 +190,21 @@ class Blades
 
         # updating the cache for reading later
         XCache::set("4e4d5752-a99a-4ed1-87b0-eb3fccb2b881:#{uuid}", filepath)
+
+        # Maintaining: #{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831
+        items = XCache::getOrNull("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831")
+        if items then
+            items = JSON.parse(items)
+            item = Blades::itemOrNull(uuid)
+            if item then
+                items[uuid] = item
+                XCache::set("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831", JSON.generate(items))
+            end
+        end
+
+        # Maintaining: @memory1
+        @memory1[uuid] = Blades::itemOrNull(uuid)
+
         nil
     end
 
@@ -195,8 +217,8 @@ class Blades
         }
     end
 
-    # Blades::items_enumerator()
-    def self.items_enumerator()
+    # Blades::itemsEnumeratorUseTheForce()
+    def self.itemsEnumeratorUseTheForce()
         Enumerator.new do |items|
             Blades::filepaths_enumerator().each{|filepath|
                 item = Blades::filepathToItem(filepath)
@@ -205,10 +227,37 @@ class Blades
         end
     end
 
+    # Blades::items()
+    def self.items()
+
+        # We try @memory1
+        if @memory1.values.size > 0 then
+            return @memory1.values
+        end
+
+        # We try XCache
+        data = XCache::getOrNull("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831")
+        if data then
+            data = JSON.parse(data)
+            @memory1 = data
+            return @memory1.values
+        end
+
+        data = {}
+        Blades::itemsEnumeratorUseTheForce().each{|item|
+            data[item["uuid"]] = item
+        }
+
+        @memory1 = data
+        XCache::set("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831", JSON.generate(data))
+
+        @memory1.values
+    end
+
     # Blades::mikuTypes()
     def self.mikuTypes()
         mikuTypes = []
-        Blades::items_enumerator().each{|item|
+        Blades::items().each{|item|
             mikuTypes << item["mikuType"]
         }
         mikuTypes.uniq
@@ -216,13 +265,25 @@ class Blades
 
     # Blades::mikuType(mikuType) -> Array[Item]
     def self.mikuType(mikuType)
-        Blades::items_enumerator().select{|item| item["mikuType"] == mikuType }
+        Blades::items().select{|item| item["mikuType"] == mikuType }
     end
 
     # Blades::deleteItem(uuid)
     def self.deleteItem(uuid)
         Blades::setAttribute(uuid, "unixtime", Time.new.to_i)
         Blades::setAttribute(uuid, "mikuType", 'NxDeleted')
+
+        # Delete from XCache
+        items = XCache::getOrNull("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831")
+        if items then
+            items = JSON.parse(items)
+            items.delete(uuid)
+            XCache::set("#{BladesConfig::cache_prefix()}:44a38835-4c00-4af9-a3c6-d5340b202831", JSON.generate(items))
+        end
+
+        # Delete from @memory1
+        @memory1.delete(uuid)
+        nil
     end
 
     # --------------------------------------------------------------------------
